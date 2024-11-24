1ed5450364b414768ce2f98ef016fb525d52ef29
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-11-24 04:40:30 +0100

    initial commit

---
+++ src/errors.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+pub enum MusiError {
+    Lexical(LexicalError),
+}
+
+#[derive(Debug)]
+pub struct LexicalError {
+    pub message: &'static str,
+}
+
+pub type MusiResult<T> = Result<T, MusiError>;
+
+impl std::error::Error for MusiError {}
+
+impl std::fmt::Display for MusiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MusiError::Lexical(error) => write!(f, "Lexical Error: {}", error.message),
+        }
+    }
+}
