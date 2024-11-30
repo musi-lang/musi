9e2921b55009956b3211e7c4ffc512cd9513ee11
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-11-30 14:24:40 +0100

    prep for parser

--- src/errors.rs
+++ src/errors.rs
@@ -1,10 +1,16 @@
 #[derive(Debug)]
 pub enum MusiError {
     Lexical(LexicalError),
+    Syntax(SyntaxError),
 }
 
 #[derive(Debug)]
 pub struct LexicalError {
+    pub message: &'static str,
+}
+
+#[derive(Debug)]
+pub struct SyntaxError {
     pub message: &'static str,
 }
 
@@ -16,6 +22,7 @@ impl std::fmt::Display for MusiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Lexical(error) => write!(f, "Lexical Error: {}", error.message),
+            Self::Syntax(error) => write!(f, "Syntax Error: {}", error.message),
         }
     }
 }
