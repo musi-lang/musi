de4938732c43bcbb396407ebcb0273019671a4c4
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-12-10 21:14:31 +0200

    fetching name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use common::{errors::MusiResult, source::Source};
+use fe::lexer::Lexer;
+
 mod be;
 mod common;
 mod fe;
@@ -6,9 +9,15 @@ mod me;
 fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
 
-    let input = std::fs::read_to_string("examples/conditional.musi").unwrap();
-    let mut lexer = crate::fe::lexer::Lexer::new(input.as_bytes());
-    let tokens = lexer.lex();
+    let path = std::path::PathBuf::from("examples/conditional.musi");
+    let name = path.file_stem().unwrap().to_string_lossy().to_string();
+    let content = std::fs::read_to_string(&path).unwrap();
+    let source = Source::new(&name, content.into());
+    let mut lexer = Lexer::new(source.into());
+    let tokens = lexer.lex().unwrap_or_else(|error| {
+        eprintln!("{error}");
+        std::process::exit(1);
+    });
     for token in tokens {
         println!("{token:?}");
     }
