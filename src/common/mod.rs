fe6595ebe45cf3bedfcc42bd9010246bd8f7b39e
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-12-10 21:15:33 +0200

    chars to COMMON

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,5 +1,13 @@
 pub mod errors;
-pub mod location;
-pub mod span;
+pub mod source;
 pub mod token;
 pub mod value;
+
+pub const CHAR_QUOTE: u8 = b'"';
+pub const CHAR_APOSTROPHE: u8 = b'\'';
+pub const CHAR_BACKSLASH: u8 = b'\\';
+pub const CHAR_UNDERSCORE: u8 = b'_';
+pub const CHAR_DOT: u8 = b'.';
+pub const CHAR_CR: u8 = b'\r';
+pub const CHAR_LF: u8 = b'\n';
+pub const CHAR_ZERO: u8 = b'0';
