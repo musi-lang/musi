c6a5ef4063b10a32daea5d2b1074660332e85877
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-12-10 21:26:15 +0200

    will go BIN later

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
-use common::source::Source;
-use fe::lexer::Lexer;
-
-mod be;
-mod common;
-mod fe;
-mod me;
-
 fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
 
     let path = std::path::PathBuf::from("examples/conditional.musi");
     let name = path.file_stem().unwrap().to_string_lossy().to_string();
     let content = std::fs::read_to_string(&path).unwrap();
-    let source = Source::new(&name, content.into());
-    let mut lexer = Lexer::new(source.into());
+    let source = musi::source::Source::new(&name, content.into());
+    let mut lexer = musi::fe::lexer::Lexer::new(source.into());
     let tokens = lexer.lex().unwrap_or_else(|error| {
         eprintln!("{error}");
         std::process::exit(1);
