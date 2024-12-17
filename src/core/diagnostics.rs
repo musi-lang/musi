ab249fcf632698a0b1eb601f041c9d5ca8192b3b
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-12-17 21:02:16 +0200

    java-style error with small rust-style

--- src/core/diagnostics.rs
+++ src/core/diagnostics.rs
@@ -1,6 +1,12 @@
 use super::{source::NamedSource, span::Span};
 use std::fmt::{self, Write as _};
 
+const BOLD: &str = "\x1b[1m";
+const RED: &str = "\x1b[31m";
+const YELLOW: &str = "\x1b[33m";
+const BLUE: &str = "\x1b[34m";
+const RESET: &str = "\x1b[0m";
+
 #[derive(Debug, Clone, Copy)]
 #[non_exhaustive]
 pub enum Severity {
@@ -8,11 +14,22 @@ pub enum Severity {
     Warning,
 }
 
+impl fmt::Display for Severity {
+    #[expect(clippy::min_ident_chars, reason = "rustc forbids changing it")]
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::Error => write!(f, "{RED}error{RESET}"),
+            Self::Warning => write!(f, "{YELLOW}warning{RESET}"),
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Diagnostic {
     severity: Severity,
-    primary_message: String,
-    help_message: Option<String>,
+    message: String,
+    note_message: Option<String>,
     span: Span,
     source: Option<Box<NamedSource>>,
 }
@@ -22,8 +39,8 @@ impl Diagnostic {
     pub fn error<M: Into<String>>(message: M, span: Span) -> Self {
         Self {
             severity: Severity::Error,
-            primary_message: message.into(),
-            help_message: None,
+            message: message.into(),
+            note_message: None,
             span,
             source: None,
         }
@@ -33,8 +50,8 @@ impl Diagnostic {
     pub fn warning<M: Into<String>>(message: M, span: Span) -> Self {
         Self {
             severity: Severity::Warning,
-            primary_message: message.into(),
-            help_message: None,
+            message: message.into(),
+            note_message: None,
             span,
             source: None,
         }
@@ -42,8 +59,8 @@ impl Diagnostic {
 
     #[inline]
     #[must_use]
-    pub fn with_help<M: Into<String>>(mut self, message: M) -> Self {
-        self.help_message = Some(message.into());
+    pub fn with_note<M: Into<String>>(mut self, message: M) -> Self {
+        self.note_message = Some(message.into());
         self
     }
 
@@ -55,58 +72,79 @@ impl Diagnostic {
     }
 
     fn format(&self) -> String {
-        let mut formatted_message = String::new();
+        let mut output = String::new();
 
         if let Some(source) = self.source.as_ref() {
-            let start = source.position(self.span.start);
+            let (start_position, end_position) = (
+                source.position(self.span.start),
+                source.position(self.span.end),
+            );
 
             writeln!(
-                formatted_message,
-                " --> {}:{}:{}",
-                source.file_path, start.line, start.column
+                output,
+                "{}:{}: {BOLD}{}{BOLD}: {BOLD}{}{RESET}",
+                source.file_path, start_position.line, self.severity, self.message
             )
             .unwrap();
 
-            writeln!(formatted_message, "error: {}", self.primary_message).unwrap();
+            writeln!(output, "    {BLUE}{BOLD}|{RESET}").unwrap();
 
-            let line_start_offset = source
-                .line_offsets
-                .get(
-                    usize::try_from(start.line)
-                        .unwrap_or_default()
-                        .saturating_sub(1),
-                )
-                .copied()
-                .unwrap_or(0);
+            let line = usize::try_from(start_position.line)
+                .unwrap_or_default()
+                .saturating_sub(1);
 
-            let line_end_offset = source
-                .line_offsets
-                .get(usize::try_from(start.line).unwrap_or_default())
-                .copied()
-                .unwrap_or(source.bytes.len());
+            let (current_line_start_offset, current_line_end_offset) = (
+                source.line_offsets.get(line).copied().unwrap_or(0),
+                source
+                    .line_offsets
+                    .get(line.saturating_add(1))
+                    .copied()
+                    .unwrap_or(source.bytes.len()),
+            );
 
             let source_line = String::from_utf8_lossy(
                 source
                     .bytes
-                    .get(line_start_offset..line_end_offset)
+                    .get(current_line_start_offset..current_line_end_offset)
                     .unwrap_or_default(),
             );
-            writeln!(formatted_message, "{:>4} | {}", start.line, source_line).unwrap();
-
-            let error_market_offset = usize::try_from(start.column).unwrap_or(1).saturating_sub(1);
             writeln!(
-                formatted_message,
-                "     | {}^",
-                " ".repeat(error_market_offset)
+                output,
+                "{BLUE}{BOLD}{:3}{RESET} {BLUE}{BOLD}|{RESET} {}",
+                start_position.column, source_line
             )
             .unwrap();
 
-            if let Some(message) = self.help_message.as_ref() {
-                writeln!(formatted_message, "help: {message}").unwrap();
+            let start_column = start_position.column.saturating_sub(1);
+            let end_column = end_position.column.saturating_sub(1);
+            let marker = format!(
+                "{}{}{}",
+                match self.severity {
+                    Severity::Error => RED,
+                    Severity::Warning => YELLOW,
+                },
+                "^".repeat(
+                    end_column
+                        .saturating_sub(start_column)
+                        .saturating_add(1)
+                        .try_into()
+                        .unwrap_or_default(),
+                ),
+                RESET
+            );
+            writeln!(
+                output,
+                "    {BLUE}{BOLD}|{RESET} {}{BOLD}{marker}{RESET}",
+                " ".repeat(start_column.try_into().unwrap_or_default())
+            )
+            .unwrap();
+
+            if let Some(note) = self.note_message.as_ref() {
+                writeln!(output, "    {BLUE}{BOLD}={RESET} {BOLD}note:{RESET} {note}").unwrap();
             }
         }
 
-        formatted_message
+        output
     }
 }
 
