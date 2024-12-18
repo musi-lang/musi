6ebaa3b5335ec93df1769d6f8434f0f7060c5de1
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-12-18 03:45:40 +0200

    cleaner ast

--- src/analysis/syntactic/ast.rs
+++ src/analysis/syntactic/ast.rs
@@ -3,121 +3,104 @@ use crate::{
     core::{span::Span, value::Value},
 };
 
-#[derive(Debug)]
 #[non_exhaustive]
+#[derive(Debug)]
 pub struct Program {
     pub statements: Vec<Statement>,
     pub span: Span,
 }
 
-#[derive(Debug)]
 #[non_exhaustive]
+#[derive(Debug)]
 pub struct Statement {
     pub kind: StatementKind,
     pub span: Span,
 }
 
-#[derive(Debug)]
 #[non_exhaustive]
+#[derive(Debug)]
+pub enum StatementKind {
+    Declaration(Declaration),
+    Expression(Expression),
+    While {
+        condition: Box<Expression>,
+        body: Box<Statement>,
+    },
+    Return(Option<Box<Expression>>),
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub enum Declaration {
+    Variable {
+        name: Box<str>,
+        mutable: bool,
+        type_annotation: Option<TypeAnnotation>,
+        initialiser: Option<Expression>,
+    },
+    Function {
+        name: Box<str>,
+        parameters: Vec<Parameter>,
+        return_type: Option<TypeAnnotation>,
+        body: Box<Statement>,
+    },
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
 pub struct Expression {
     pub kind: ExpressionKind,
+    pub type_annotation: Option<TypeAnnotation>,
     pub span: Span,
 }
 
-#[derive(Debug)]
 #[non_exhaustive]
-pub enum StatementKind {
-    Expression(Expression),
-    Block(Vec<Statement>),
-    VariableDeclaration {
-        name: Box<str>,
-        initialiser: Expression,
-        mutable: bool,
-        span: Span,
-    },
-    // FunctionDeclaration {
-    //     name: Box<str>,
-    //     parameters: Vec<Box<str>>,
-    //     body: Box<Statement>,
-    //     span: Span,
-    // },
-}
-
 #[derive(Debug)]
-#[non_exhaustive]
 pub enum ExpressionKind {
-    Assignment {
-        target: Box<Expression>,
-        value: Box<Expression>,
-        span: Span,
-    },
+    Literal(Value),
+    Variable(Box<str>),
     Binary {
         left: Box<Expression>,
         operator: TokenKind,
         right: Box<Expression>,
-        span: Span,
-    },
-    Block {
-        statements: Vec<Statement>,
-        nesting_depth: u8,
-        span: Span,
-    },
-    Call {
-        callee: Box<Expression>,
-        arguments: Vec<Expression>,
-        span: Span,
-    },
-    Conditional {
-        condition: Box<Expression>,
-        consequence: Box<Expression>,
-        alternative: Option<Box<Expression>>,
-        span: Span,
-    },
-    Identifier {
-        name: Box<str>,
-        span: Span,
-    },
-    Lambda {
-        parameters: Vec<Box<str>>,
-        body: Box<Expression>,
-        span: Span,
-    },
-    Literal {
-        value: Value,
-        span: Span,
-    },
-    Match {
-        scrutinee: Box<Expression>,
-        arms: Vec<MatchArm>,
-        span: Span,
     },
     Unary {
         operator: TokenKind,
         operand: Box<Expression>,
-        span: Span,
+    },
+    Call {
+        callee: Box<Expression>,
+        arguments: Vec<Expression>,
+    },
+    Lambda {
+        parameters: Vec<Parameter>,
+        body: Box<Expression>,
+    },
+    Assignment {
+        target: Box<Expression>,
+        value: Box<Expression>,
+    },
+    Block {
+        statements: Vec<Statement>,
+    },
+    If {
+        condition: Box<Expression>,
+        then_branch: Box<Expression>,
+        else_branch: Option<Box<Expression>>,
     },
 }
 
-#[derive(Debug)]
 #[non_exhaustive]
-pub enum PatternKind {
-    Literal(Value),
-    Identifier(Box<str>),
-    Tuple {
-        elements: Vec<PatternKind>,
-    },
-    Constructor {
-        name: Box<str>,
-        fields: Vec<PatternKind>,
-    },
-    Wildcard,
+#[derive(Debug)]
+pub struct Parameter {
+    pub name: Box<str>,
+    pub type_annotation: Option<TypeAnnotation>,
 }
 
-#[derive(Debug)]
 #[non_exhaustive]
-pub struct MatchArm {
-    pub pattern: PatternKind,
-    pub guard: Option<Box<Expression>>,
-    pub body: Box<Expression>,
+#[derive(Debug)]
+pub struct TypeAnnotation {
+    pub name: Box<str>,
+    pub parameters: Vec<TypeAnnotation>,
     pub span: Span,
 }
