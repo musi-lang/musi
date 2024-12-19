3db0189b1837e7a2f3773832e22d7d9bc90e7804
kawaffee <187086553+kawaffee@users.noreply.github.com>
2024-12-19 23:43:58 +0200

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
-use musi::{analysis::lexical::lexer::Lexer, core::source::Source};
+#![allow(clippy::all, reason = "development")]
+#![allow(clippy::restriction, reason = "development")]
+
+use std::path::PathBuf;
+
+use musi::{
+    analysis::{lexical::lexer::Lexer, syntactic::parser::Parser},
+    core::source::SourceFile,
+};
 
 fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
-
-    let path = std::path::PathBuf::from("examples/main.musi");
-    let name = path.file_stem().unwrap().to_string_lossy().to_string();
+    let path = PathBuf::from("examples/main.musi");
+    let name = path.file_name().unwrap().to_string_lossy().to_string();
     let content = std::fs::read_to_string(&path).unwrap();
-    let source = Source::new(&name, content.into());
+    let source = SourceFile::new(&name, content.into());
+
     let mut lexer = Lexer::new(source.into());
     let tokens = lexer.lex().unwrap_or_else(|error| {
         eprintln!("{error}");
         std::process::exit(1);
     });
-    for token in tokens {
+    for token in &tokens {
         println!("{token:?}");
     }
+
+    let mut parser = Parser::new(tokens);
+    let program = parser.parse().unwrap_or_else(|error| {
+        eprintln!("{error}");
+        std::process::exit(1);
+    });
+    println!("{program:?}");
 }
