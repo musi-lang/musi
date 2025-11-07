8780b9a23976e6c06d6730056632fbda307427f4
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-07 09:21:17 +0200

    refactor(pipeline): make `setup_pipeline` and `check_source` use filename for diagnostics

--- bin/pipeline.ml
+++ bin/pipeline.ml
@@ -1,9 +1,9 @@
 (** Compilation pipeline orchestrating all compiler stages. *)
 
-let setup_pipeline source =
+let setup_pipeline filename source =
   let interner = Interner.create () in
   let file_id = 0 in
-  let lexer = Lexer.make file_id source interner in
+  let lexer = Lexer.make file_id filename source interner in
   let tokens, lex_diags = Lexer.lex lexer in
   let parser = Parser.make tokens interner in
   let ast, parse_diags = Parser.parse parser in
@@ -13,9 +13,9 @@ let setup_pipeline source =
   let check_diags = Checker.check checker ast in
   (interner, ast, lex_diags, parse_diags, resolve_diags, check_diags)
 
-let check_source source =
+let check_source filename source =
   let _, _, lex_diags, parse_diags, resolve_diags, check_diags =
-    setup_pipeline source
+    setup_pipeline filename source
   in
   Diagnostic.merge [ lex_diags; parse_diags; resolve_diags; check_diags ]
 
@@ -23,10 +23,10 @@ let check_file input_path =
   let ic = open_in input_path in
   let source = really_input_string ic (in_channel_length ic) in
   close_in ic;
-  check_source source
+  check_source input_path source
 
-let compile_source source =
-  let interner, ast, _, _, _, _ = setup_pipeline source in
+let compile_source filename source =
+  let interner, ast, _, _, _, _ = setup_pipeline filename source in
   let proc_table = Emitter.collect_procs interner ast in
   let diags = ref Diagnostic.empty_bag in
   let procs, module_desc = Emitter.emit_program interner proc_table diags ast in
@@ -37,12 +37,68 @@ let compile_file input_path output_path =
   let ic = open_in input_path in
   let source = really_input_string ic (in_channel_length ic) in
   close_in ic;
-  let bytecode = compile_source source in
+  let bytecode = compile_source input_path source in
   let oc = open_out_bin output_path in
   output_bytes oc bytecode;
   close_out oc
 
-let print_diagnostics diags source =
+let print_diagnostics diags filename source =
   let files = Source.empty in
-  let _, files = Source.add_file files "<input>" source in
+  let _, files = Source.add_file files filename source in
   Diagnostic.emit_all Format.err_formatter diags files
+
+let compile_module input_path output_path =
+  let interner = Interner.create () in
+  let linker = Linker.create interner in
+  let modules, link_diags = Linker.build_import_graph linker input_path in
+  if Diagnostic.has_errors link_diags then (
+    let ic = open_in input_path in
+    let source = really_input_string ic (in_channel_length ic) in
+    close_in ic;
+    print_diagnostics link_diags input_path source;
+    failwith "linking failed");
+  let sorted_modules, sort_diags = Linker.topological_sort linker modules in
+  if Diagnostic.has_errors sort_diags then (
+    let ic = open_in input_path in
+    let source = really_input_string ic (in_channel_length ic) in
+    close_in ic;
+    print_diagnostics sort_diags input_path source;
+    failwith "dependency cycle detected");
+  let resolver = Resolver.create interner in
+  List.iter
+    (fun (m : Linker.module_info) ->
+      let resolve_diags = Resolver.resolve resolver m.ast in
+      if Diagnostic.has_errors resolve_diags then (
+        let ic = open_in m.path in
+        let source = really_input_string ic (in_channel_length ic) in
+        close_in ic;
+        print_diagnostics resolve_diags m.path source;
+        failwith "resolution failed"))
+    sorted_modules;
+  let proc_table = Hashtbl.create 64 in
+  List.iter
+    (fun (m : Linker.module_info) ->
+      let m_procs = Emitter.collect_procs interner m.ast in
+      Hashtbl.iter (fun k v -> Hashtbl.replace proc_table k v) m_procs)
+    sorted_modules;
+  List.iter
+    (fun (m : Linker.module_info) ->
+      let diags = ref Diagnostic.empty_bag in
+      let procs, module_desc =
+        Emitter.emit_program interner proc_table diags m.ast
+      in
+      if Diagnostic.has_errors !diags then (
+        let ic = open_in m.path in
+        let source = really_input_string ic (in_channel_length ic) in
+        close_in ic;
+        print_diagnostics !diags m.path source;
+        failwith "codegen failed");
+      let bytecode = Encoder.encode_program procs module_desc in
+      let out_path =
+        if m.path = input_path then output_path
+        else String.sub m.path 0 (String.length m.path - 3) ^ ".msc"
+      in
+      let oc = open_out_bin out_path in
+      output_bytes oc bytecode;
+      close_out oc)
+    sorted_modules
--- bin/pipeline.mli
+++ bin/pipeline.mli
@@ -3,12 +3,12 @@
 (** Check source text through pipeline.
 
     STAGES: Lexer -> Parser -> Resolver -> Checker *)
-val check_source : string -> Diagnostic.bag
+val check_source : string -> string -> Diagnostic.bag
 
 (** Compile source text to bytecode.
 
     STAGES: Lexer -> Parser -> Resolver -> Checker -> Codegen *)
-val compile_source : string -> bytes
+val compile_source : string -> string -> bytes
 
 (** Read source file and check. *)
 val check_file : string -> Diagnostic.bag
@@ -16,5 +16,8 @@ val check_file : string -> Diagnostic.bag
 (** Compile source file to [<output>.msc] bytecode file. *)
 val compile_file : string -> string -> unit
 
+(** Compile module with dependency resolution and linking. *)
+val compile_module : string -> string -> unit
+
 (** Format and write diagnostics with source context to stderr. *)
-val print_diagnostics : Diagnostic.bag -> string -> unit
+val print_diagnostics : Diagnostic.bag -> string -> string -> unit
