85f5cf9e97d2658d91c144e62186f7ebb6d2cae7
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-07 10:56:34 +0200

    feat(Int): impl `from_value` method and def Int record

---
+++ runtime/src/lang/_Int.cpp
@@ -0,0 +1,9 @@
+#include "_Int.hpp"
+
+namespace musi {
+
+  auto Int::from_value(int64_t value) -> int64_t {
+    return value;
+  }
+
+}  // namespace musi
---
+++ runtime/src/lang/_Int.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace musi {
+
+  class Int {
+public:
+    static auto from_value(int64_t value) -> int64_t;
+  };
+
+}  // namespace musi
