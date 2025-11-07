35345b11d1459f06bd5eba138fa180a68709bbb8
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-07 15:17:32 +0200

    refactor(decoder): remove unused export and link handling; switch to metadata

--- lib/codegen/decoder.ml
+++ lib/codegen/decoder.ml
@@ -14,46 +14,19 @@ let decode_header buf =
       let version = Binary.read_u32_le buf 4 in
       let bc_offset = Binary.read_u32_le buf 8 in
       let bc_size = Binary.read_u32_le buf 12 in
-      let export_offset = Binary.read_u32_le buf 16 in
-      let export_count = Binary.read_u32_le buf 20 in
-      let link_offset = Binary.read_u32_le buf 24 in
-      let link_count = Binary.read_u32_le buf 28 in
+      let metadata_offset = Binary.read_u32_le buf 16 in
+      let metadata_size = Binary.read_u32_le buf 20 in
+      let reserved = Binary.read_i64_le buf 24 in
       Ok
         {
           Metadata.magic
         ; version
         ; bc_offset
         ; bc_size
-        ; export_offset
-        ; export_count
-        ; link_offset
-        ; link_count
+        ; metadata_offset
+        ; metadata_size
+        ; reserved
         }
-
-let decode_string buf offset =
-  let len = Binary.read_u16_le buf offset in
-  let str = Bytes.sub_string buf (offset + 2) len in
-  (str, offset + 2 + len)
-
-let decode_exports buf offset count =
-  let rec loop acc pos remaining =
-    if remaining = 0 then Ok (List.rev acc)
-    else
-      let name, next_pos = decode_string buf pos in
-      let proc_id = Binary.read_u16_le buf next_pos in
-      loop ((name, proc_id) :: acc) (next_pos + 2) (remaining - 1)
-  in
-  loop [] (Int32.to_int offset) (Int32.to_int count)
-
-let decode_links buf offset count =
-  let rec loop acc pos remaining =
-    if remaining = 0 then Ok (List.rev acc)
-    else
-      let proc_id = Binary.read_u16_le buf pos in
-      let name, next_pos = decode_string buf (pos + 2) in
-      loop ((proc_id, name) :: acc) next_pos (remaining - 1)
-  in
-  loop [] (Int32.to_int offset) (Int32.to_int count)
 
 let decode_program buf =
   match decode_header buf with
@@ -65,14 +38,8 @@ let decode_program buf =
       Error "bytecode section out of bounds"
     else
       let bytecode = Bytes.sub buf bc_start bc_len in
-      let exports =
-        if header.export_count = 0l then Ok []
-        else decode_exports buf header.export_offset header.export_count
-      in
-      let links =
-        if header.link_count = 0l then Ok []
-        else decode_links buf header.link_offset header.link_count
-      in
+      let exports = Ok [] in
+      let links = Ok [] in
       match (exports, links) with
       | Ok exports, Ok links -> Ok { header; bytecode; exports; links }
       | Error e, _ -> Error e
