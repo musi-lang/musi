679934b0110a3f1206a2abcbfc1a79cb3362a943
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-07 12:59:49 +0200

    refactor(test): switch from `LdcI4_1` to `LdcI4 1l`

--- lib/codegen/test_encoder.ml
+++ lib/codegen/test_encoder.ml
@@ -24,7 +24,7 @@ let test_encode_single_instruction () =
   check bool "has bytecode" true (Bytes.length bytes > header_size)
 
 let test_encode_multiple_instructions () =
-  let instrs = [ Instr.LdcI4_1; Instr.LdcI4_1; Instr.Add; Instr.Ret ] in
+  let instrs = [ Instr.LdcI4 1l; Instr.LdcI4 1l; Instr.Add; Instr.Ret ] in
   let bytes = Encoder.encode_program [| instrs |] empty_module_desc in
   check bool "has bytecode" true (Bytes.length bytes > header_size)
 
