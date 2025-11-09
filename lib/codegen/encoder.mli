6b98263bd34484f7492518de9233ca7d6ee6f3ae
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-09 08:45:29 +0200

    remove old

--- lib/codegen/encoder.mli
+++
@@ -1,4 +0,0 @@
-(** Provides helpers that serialise compiled bytecode into [<output>.msil] file format. *)
-
-(** Serialises provided array of procedure bytecode into [<output>.msil] payload. *)
-val encode_program : Instr.t list array -> Metadata.module_desc -> bytes
