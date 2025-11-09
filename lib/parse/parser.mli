2c2ab55bc17870f067313d84fd418a5596f9e578
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-09 12:43:00 +0200

    chore(parse): remove unused parse library and related files

--- lib/parse/parser.mli
+++
@@ -1,8 +0,0 @@
-(** Represents mutable state used while parsing modules. *)
-type t
-
-(** Constructs parser from provided token stream and interner. *)
-val make : Token.t list -> Interner.t -> t
-
-(** Parses complete module and returns AST plus diagnostics. *)
-val parse : t -> Node.t list * Diagnostic.bag
