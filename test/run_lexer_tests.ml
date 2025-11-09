7906e43d601cb8b30b179bb985f1b46ece01f379
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-09 13:15:27 +0200

    test: fix diagnostics output in lexer and parser tests

--- test/run_lexer_tests.ml
+++ test/run_lexer_tests.ml
@@ -4,17 +4,19 @@ open Musi_lex
 let test_pass path () =
   let source = Test_helpers.read_file path in
   let interner = Interner.create () in
-  let file_id, _ = Source.add_file Source.empty path source in
+  let file_id, src = Source.add_file Source.empty path source in
   let lexer = Lexer.make file_id source interner in
   let _, diags = Lexer.lex_all lexer in
+  Diagnostic.emit_all Format.std_formatter diags src;
   Alcotest.(check bool) "no errors" false (Diagnostic.has_errors diags)
 
 let test_fail path () =
   let source = Test_helpers.read_file path in
   let interner = Interner.create () in
-  let file_id, _ = Source.add_file Source.empty path source in
+  let file_id, src = Source.add_file Source.empty path source in
   let lexer = Lexer.make file_id source interner in
   let _, diags = Lexer.lex_all lexer in
+  Diagnostic.emit_all Format.std_formatter diags src;
   Alcotest.(check bool) "has errors" true (Diagnostic.has_errors diags)
 
 let () =
--- test/run_parser_tests.ml
+++ test/run_parser_tests.ml
@@ -5,23 +5,25 @@ open Musi_parse
 let test_pass path () =
   let source = Test_helpers.read_file path in
   let interner = Interner.create () in
-  let file_id, _ = Source.add_file Source.empty path source in
+  let file_id, src = Source.add_file Source.empty path source in
   let lexer = Lexer.make file_id source interner in
   let tokens, lex_diags = Lexer.lex_all lexer in
   let parser = Parser.make tokens interner in
   let _, parse_diags = Parser.parse parser in
   let diags = Diagnostic.merge [ lex_diags; parse_diags ] in
+  Diagnostic.emit_all Format.std_formatter diags src;
   Alcotest.(check bool) "no errors" false (Diagnostic.has_errors diags)
 
 let test_fail path () =
   let source = Test_helpers.read_file path in
   let interner = Interner.create () in
-  let file_id, _ = Source.add_file Source.empty path source in
+  let file_id, src = Source.add_file Source.empty path source in
   let lexer = Lexer.make file_id source interner in
   let tokens, lex_diags = Lexer.lex_all lexer in
   let parser = Parser.make tokens interner in
   let _, parse_diags = Parser.parse parser in
   let diags = Diagnostic.merge [ lex_diags; parse_diags ] in
+  Diagnostic.emit_all Format.std_formatter diags src;
   Alcotest.(check bool) "has errors" true (Diagnostic.has_errors diags)
 
 let () =
