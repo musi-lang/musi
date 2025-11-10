a949e064af8369301ba4923d0c1ef4defe0914e8
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-10 22:30:11 +0200

    feat(tests): add resolver integration tests

---
+++ test/run_resolver_tests.ml
@@ -0,0 +1,40 @@
+open Musi_basic
+open Musi_lex
+open Musi_parse
+open Musi_sema
+
+let test_pass path () =
+  let source = Test_helpers.read_file path in
+  let interner = Interner.create () in
+  let file_id, src = Source.add_file Source.empty path source in
+  let lexer = Lexer.make file_id source interner in
+  let tokens, lex_diags = Lexer.lex_all lexer in
+  let parser = Parser.make tokens interner in
+  let stmts, parse_diags = Parser.parse parser in
+  let resolve_diags = ref Diagnostic.empty_bag in
+  let _ = Resolver.resolve stmts interner resolve_diags in
+  let diags = Diagnostic.merge [ lex_diags; parse_diags; !resolve_diags ] in
+  Diagnostic.emit_all Format.std_formatter diags src;
+  Alcotest.(check bool) "no errors" false (Diagnostic.has_errors diags)
+
+let test_fail path () =
+  let source = Test_helpers.read_file path in
+  let interner = Interner.create () in
+  let file_id, src = Source.add_file Source.empty path source in
+  let lexer = Lexer.make file_id source interner in
+  let tokens, lex_diags = Lexer.lex_all lexer in
+  let parser = Parser.make tokens interner in
+  let stmts, parse_diags = Parser.parse parser in
+  let resolve_diags = ref Diagnostic.empty_bag in
+  let _ = Resolver.resolve stmts interner resolve_diags in
+  let diags = Diagnostic.merge [ lex_diags; parse_diags; !resolve_diags ] in
+  Diagnostic.emit_all Format.std_formatter diags src;
+  Alcotest.(check bool) "has errors" true (Diagnostic.has_errors diags)
+
+let () =
+  let pass_files = Test_helpers.collect_files "resolver/pass" in
+  let fail_files = Test_helpers.collect_files "resolver/fail" in
+  let tests =
+    Test_helpers.make_test_cases pass_files fail_files test_pass test_fail
+  in
+  Alcotest.run "Resolver Integration" tests
