ffe407fd72bcffe58c0ba37ef6db39bdb393e3bf
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-11 19:34:12 +0200

    feat(emitter): add initial emitter module with const_kind type and emit functions

---
+++ lib/musi_codegen/emitter.mli
@@ -0,0 +1,14 @@
+open Musi_basic
+open Musi_parse
+
+type const_kind =
+  | ConstInt of int64
+  | ConstBin of float
+  | ConstStr of Interner.name
+  | ConstBool of bool
+  | ConstUnit
+
+type t
+
+val make : unit -> t
+val emit : t -> Node.stmt list -> Instr.t list
