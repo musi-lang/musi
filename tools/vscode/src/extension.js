32486e555a884f36c90ddef16b2d6820254b1eb5
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-17 20:14:07 +0200

    possible fix

--- tools/vscode/src/extension.js
+++ tools/vscode/src/extension.js
@@ -1,14 +1,20 @@
 import { languages } from "vscode";
 
+/**
+ * @param {{ subscriptions: import("vscode").Disposable[]; }} context
+ */
 export function activate(context) {
 	const provider = languages.registerCompletionItemProvider("musi", {
 		provideCompletionItems(_document, _position, _token, _context) {
 			return [];
 		},
 	});
-	context.subscriptions.push(provider);
+
+	if (!context.subscriptions.includes(provider)) {
+		context.subscriptions.push(provider);
+	}
 }
 
 export function deactivate() {
-	return;
+	return undefined;
 }
