0f93bdcb5fb2fbd9d315b339da595421b66c1e81
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2025-11-18 07:01:25 +0200

    remove old

--- src/decoder.ads
+++
@@ -1,9 +0,0 @@
-with Ada.Streams;
-
-package Decoder is
-
-   use Ada.Streams;
-
-   procedure Disassemble (Bytecode : Stream_Element_Array);
-
-end Decoder;
--- src/musi.adb
+++
@@ -1,8 +0,0 @@
--- New main entry point using Musi.CLI
-with Musi.CLI;
-
-procedure Musi_Main is
-   Args : constant Musi.CLI.CLI_Arguments := Musi.CLI.Parse_Arguments;
-begin
-   Musi.CLI.Execute_Command (Args);
-end Musi_Main;
