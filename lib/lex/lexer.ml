a528b13b3cc9726f2b6a617ae8561eb922a1537a
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-02 12:19:12 +0200

    fix lexer problems

--- lib/lex/lexer.ml
+++ lib/lex/lexer.ml
@@ -31,7 +31,7 @@ let add_error st msg start end_ =
 
 let add_error_code st code start end_ args =
   let sp = Span.make st.file_id start end_ in
-  { st with diags = Diagnostic.add st.diags (Lex_error.diag code sp args) }
+  { st with diags = Diagnostic.add st.diags (Lex.Error.diag code sp args) }
 
 let extract st s e = String.sub st.source s (e - s)
 
@@ -81,7 +81,7 @@ let check_utf8_char st pos =
   else
     let first = Char.code st.source.[pos] in
     if first < 0x80 then (pos + 1, None)
-    else if first < 0xC0 then (pos + 1, Some (Lex_error.E0001, pos, pos + 1))
+    else if first < 0xC0 then (pos + 1, Some (Lex.Error.E0001, pos, pos + 1))
     else
       let exp =
         if first < 0xE0 then 2
@@ -89,9 +89,9 @@ let check_utf8_char st pos =
         else if first < 0xF8 then 4
         else 0
       in
-      if exp = 0 then (pos + 1, Some (Lex_error.E0002, pos, pos + 1))
+      if exp = 0 then (pos + 1, Some (Lex.Error.E0002, pos, pos + 1))
       else if pos + exp > st.len then
-        (st.len, Some (Lex_error.E0003, pos, st.len))
+        (st.len, Some (Lex.Error.E0003, pos, st.len))
       else
         let rec chk idx =
           if idx >= pos + exp then None
@@ -100,7 +100,7 @@ let check_utf8_char st pos =
           else chk (idx + 1)
         in
         match chk (pos + 1) with
-        | Some inv -> (pos + exp, Some (Lex_error.E0001, inv, inv + 1))
+        | Some inv -> (pos + exp, Some (Lex.Error.E0001, inv, inv + 1))
         | None -> (pos + exp, None)
 
 let check_utf8_in_range st sp ep =
@@ -177,7 +177,7 @@ let scan_number_chars st start valid base =
       let c = st.source.[p] in
       if valid c then loop (p + 1) s
       else if is_alpha c || is_digit c then
-        (p, add_error_code s Lex_error.E0101 p (p + 1) [ base; String.make 1 c ])
+        (p, add_error_code s Lex.Error.E0101 p (p + 1) [ base; String.make 1 c ])
       else (p, s)
   in
   loop start st
@@ -190,7 +190,7 @@ let scan_decimal st start =
       if is_digit c then loop (pos + 1) s dots
       else if c = '.' then
         if dots > 0 then
-          (pos, add_error_code s Lex_error.E0102 pos pos [], dots + 1)
+          (pos, add_error_code s Lex.Error.E0102 pos pos [], dots + 1)
         else loop (pos + 1) s (dots + 1)
       else (pos, s, dots)
   in
@@ -200,7 +200,7 @@ let scan_decimal st start =
 let scan_based_number st start pfx valid base =
   let pos = st.pos + pfx in
   let mkErr s =
-    { (add_error_code s Lex_error.E0103 start pos [ base ]) with pos }
+    { (add_error_code s Lex.Error.E0103 start pos [ base ]) with pos }
   in
   if pos >= st.len then (mkErr st, extract st start pos, mk_span st start)
   else
@@ -256,7 +256,7 @@ let parse_unicode_escape st pos =
       | Some eb -> (
         let hex = String.sub st.source (pos + 3) (eb - pos - 3) in
         if String.length hex = 0 then
-          Some (add_error_code st Lex_error.E0205 pos (eb + 1) [], eb + 1)
+          Some (add_error_code st Lex.Error.E0205 pos (eb + 1) [], eb + 1)
         else
           try
             let v = int_of_string ("0x" ^ hex) in
@@ -265,21 +265,21 @@ let parse_unicode_escape st pos =
               Some
                 ( add_error_code
                     st
-                    Lex_error.E0207
+                    Lex.Error.E0207
                     pos
                     (eb + 1)
                     [ Printf.sprintf "%X" max_v ]
                 , eb + 1 )
             else None
           with _ ->
-            Some (add_error_code st Lex_error.E0208 pos (eb + 1) [], eb + 1))
+            Some (add_error_code st Lex.Error.E0208 pos (eb + 1) [], eb + 1))
       | None ->
         if pos + 3 < st.len then
-          Some (add_error_code st Lex_error.E0209 pos (pos + 4) [], pos + 3)
-        else Some (add_error_code st Lex_error.E0206 pos st.len [], st.len)
+          Some (add_error_code st Lex.Error.E0209 pos (pos + 4) [], pos + 3)
+        else Some (add_error_code st Lex.Error.E0206 pos st.len [], st.len)
     else
       Some
-        ( add_error_code st Lex_error.E0210 pos (min (pos + 3) st.len) []
+        ( add_error_code st Lex.Error.E0210 pos (min (pos + 3) st.len) []
         , pos + 2 )
   else None
 
@@ -288,13 +288,13 @@ let validate_escapes st start ep =
     if pos >= ep - 1 then s
     else if st.source.[pos] = '\\' then
       if pos + 1 >= ep - 1 then
-        add_error_code s Lex_error.E0211 pos (pos + 1) []
+        add_error_code s Lex.Error.E0211 pos (pos + 1) []
       else if not (is_valid_escape st pos) then
         loop
           (pos + 2)
           (add_error_code
              s
-             Lex_error.E0212
+             Lex.Error.E0212
              pos
              (pos + 2)
              [ String.make 1 st.source.[pos + 1] ])
@@ -323,10 +323,10 @@ let scan_line_comment st =
 let scan_block_comment st =
   let start = st.pos in
   let rec loop p s =
-    if p + 1 >= st.len then (p, add_error_code s Lex_error.E0401 start st.pos [])
+    if p + 1 >= st.len then (p, add_error_code s Lex.Error.E0401 start st.pos [])
     else if st.source.[p] = '*' && st.source.[p + 1] = '/' then (p + 2, s)
     else if st.source.[p] = '/' && st.source.[p + 1] = '*' then
-      loop (p + 2) (add_error_code s Lex_error.E0402 p (p + 2) [])
+      loop (p + 2) (add_error_code s Lex.Error.E0402 p (p + 2) [])
     else loop (p + 1) s
   in
   let sc = st.pos + 2 in
@@ -343,7 +343,7 @@ let scan_ident st =
       else if Char.code c > 127 then
         loop
           (p + 1)
-          (add_error_code s Lex_error.E0304 p (p + 1) [ String.make 1 c ])
+          (add_error_code s Lex.Error.E0304 p (p + 1) [ String.make 1 c ])
       else (p, s)
   in
   let ep, fs = loop st.pos st in
@@ -358,13 +358,13 @@ let scan_literal st quote lit_name process =
   let se =
     if lit_name = "template" then
       List.fold_left
-        (fun s p -> add_error_code s Lex_error.E0204 p (p + 1) [])
+        (fun s p -> add_error_code s Lex.Error.E0204 p (p + 1) [])
         st
         extra
     else st
   in
   if unterm || ep > st.len || (ep = st.len && st.source.[ep - 1] <> quote) then
-    (add_error_code se Lex_error.E0201 start st.pos [ lit_name ], None, sp)
+    (add_error_code se Lex.Error.E0201 start st.pos [ lit_name ], None, sp)
   else
     let content =
       extract st (if lit_name = "template" then start else st.pos + 1) (ep - 1)
@@ -390,7 +390,7 @@ let scan_rune st =
   | s, None, sp -> (s, '\000', sp)
   | s, Some proc, sp ->
     if String.length proc = 0 then
-      (add_error_code s Lex_error.E0203 st.pos (st.pos + 1) [], '\000', sp)
+      (add_error_code s Lex.Error.E0203 st.pos (st.pos + 1) [], '\000', sp)
     else if String.length proc > 1 then
       ( add_error
           s
@@ -415,7 +415,7 @@ let scan_symbol st =
   | None ->
     ( add_error_code
         (advance st)
-        Lex_error.E0301
+        Lex.Error.E0301
         st.pos
         (st.pos + 1)
         [ String.make 1 st.source.[st.pos] ]
@@ -472,14 +472,14 @@ let rec lex_token st =
     if code < 32 && c <> '\t' && c <> '\n' && c <> '\r' then
       ( add_error_code
           (advance st)
-          Lex_error.E0302
+          Lex.Error.E0302
           st.pos
           (st.pos + 1)
           [ Printf.sprintf "%02X" code ]
       , Token.Whitespace
       , mk_span st st.pos )
     else if c = '\000' then
-      ( add_error_code (advance st) Lex_error.E0303 st.pos (st.pos + 1) []
+      ( add_error_code (advance st) Lex.Error.E0303 st.pos (st.pos + 1) []
       , Token.Whitespace
       , mk_span st st.pos )
     else if code >= 0x80 then
@@ -500,7 +500,7 @@ and tokenize source file_id interner =
     | _ ->
       if ns.pos <= old then
         loop
-          (advance (add_error_code ns Lex_error.E0501 old ns.pos []))
+          (advance (add_error_code ns Lex.Error.E0501 old ns.pos []))
           ((Token.Error, sp) :: tokens)
       else loop ns ((tok, sp) :: tokens)
   in
--- lib/lex/token.ml
+++ lib/lex/token.ml
@@ -1,205 +1,295 @@
-open Basic
+module type S = sig
+  type t =
+    (* Literals *)
+    | LitNumber of string
+    | LitString of Basic.Interner.name
+    | LitRune of char
+    | LitTemplate of Basic.Interner.name
+    (* Identifiers *)
+    | Ident of Basic.Interner.name
+    (* Keywords (alphabetically) *)
+    | KwAnd
+    | KwAs
+    | KwBreak
+    | KwCase
+    | KwChoice
+    | KwCycle
+    | KwDefer
+    | KwElse
+    | KwExport
+    | KwExtern
+    | KwFn
+    | KwFor
+    | KwFrom
+    | KwIf
+    | KwImport
+    | KwIn
+    | KwIs
+    | KwMatch
+    | KwNot
+    | KwOr
+    | KwRecord
+    | KwReturn
+    | KwTrait
+    | KwUnsafe
+    | KwVal
+    | KwVar
+    | KwWhile
+    (* Operators *)
+    | LtMinus (* <- *)
+    | ColonEq (* := *)
+    | Eq (* = *)
+    | BangEq (* != *)
+    | Lt (* < *)
+    | LtEq (* <= *)
+    | Gt (* > *)
+    | GtEq (* >= *)
+    | Plus (* + *)
+    | Minus (* - *)
+    | Star (* * *)
+    | Slash (* / *)
+    | StarStar (* ** *)
+    | PipeGt (* |> *)
+    | LtLt (* << *)
+    | GtGt (* >> *)
+    | Amp (* & *)
+    | Pipe (* | *)
+    | Caret (* ^ *)
+    | Tilde (* ~ *)
+    | At (* @ *)
+    | Bang (* ! *)
+    | Dollar (* $ *)
+    (* Punctuation *)
+    | LParen (* ( *)
+    | RParen (* ) *)
+    | LBrace (* { *)
+    | RBrace (* } *)
+    | LBrack (* [ *)
+    | RBrack (* ] *)
+    | Comma (* , *)
+    | Semi (* ; *)
+    | Colon (* : *)
+    | Dot (* . *)
+    | MinusGt (* -> *)
+    | Underscore (* _ *)
+    | Question (* ? *)
+    (* Special *)
+    | EOF
+    | Newline
+    | Whitespace
+    | Comment of string
+    | Error
 
-type t =
-  (* Literals *)
-  | LitNumber of string
-  | LitString of Interner.name
-  | LitRune of char
-  | LitTemplate of Interner.name
-  (* Identifiers *)
-  | Ident of Interner.name
-  (* Keywords (alphabetically) *)
-  | KwAnd
-  | KwAs
-  | KwBreak
-  | KwCase
-  | KwChoice
-  | KwCycle
-  | KwDefer
-  | KwElse
-  | KwExport
-  | KwExtern
-  | KwFn
-  | KwFor
-  | KwFrom
-  | KwIf
-  | KwImport
-  | KwIn
-  | KwIs
-  | KwMatch
-  | KwNot
-  | KwOr
-  | KwRecord
-  | KwReturn
-  | KwTrait
-  | KwUnsafe
-  | KwVal
-  | KwVar
-  | KwWhile
-  (* Operators *)
-  | LtMinus (* <- *)
-  | ColonEq (* := *)
-  | Eq (* = *)
-  | BangEq (* != *)
-  | Lt (* < *)
-  | LtEq (* <= *)
-  | Gt (* > *)
-  | GtEq (* >= *)
-  | Plus (* + *)
-  | Minus (* - *)
-  | Star (* * *)
-  | Slash (* / *)
-  | StarStar (* ** *)
-  | PipeGt (* |> *)
-  | LtLt (* << *)
-  | GtGt (* >> *)
-  | Amp (* & *)
-  | Pipe (* | *)
-  | Caret (* ^ *)
-  | Tilde (* ~ *)
-  | At (* @ *)
-  | Bang (* ! *)
-  | Dollar (* $ *)
-  (* Punctuation *)
-  | LParen (* ( *)
-  | RParen (* ) *)
-  | LBrace (* { *)
-  | RBrace (* } *)
-  | LBrack (* [ *)
-  | RBrack (* ] *)
-  | Comma (* , *)
-  | Semi (* ; *)
-  | Colon (* : *)
-  | Dot (* . *)
-  | MinusGt (* -> *)
-  | Underscore (* _ *)
-  | Question (* ? *)
-  (* Special *)
-  | EOF
-  | Newline
-  | Whitespace
-  | Comment of string
-  | Error
+  val to_string : t -> string
+  val keyword_strings : (string * t) list
+  val symbol_strings : (string * t) list
+  val keyword_to_string : (t, string) Hashtbl.t
+  val symbol_to_string : (t, string) Hashtbl.t
+  val keywords : (string, t) Hashtbl.t
+  val symbols : (string, t) Hashtbl.t
+  val lookup_keyword : Basic.Interner.t -> string -> t
+end
 
-let keyword_strings =
-  [
-    ("and", KwAnd)
-  ; ("as", KwAs)
-  ; ("break", KwBreak)
-  ; ("case", KwCase)
-  ; ("choice", KwChoice)
-  ; ("cycle", KwCycle)
-  ; ("defer", KwDefer)
-  ; ("else", KwElse)
-  ; ("export", KwExport)
-  ; ("extern", KwExtern)
-  ; ("fn", KwFn)
-  ; ("for", KwFor)
-  ; ("from", KwFrom)
-  ; ("if", KwIf)
-  ; ("import", KwImport)
-  ; ("in", KwIn)
-  ; ("is", KwIs)
-  ; ("match", KwMatch)
-  ; ("not", KwNot)
-  ; ("or", KwOr)
-  ; ("record", KwRecord)
-  ; ("return", KwReturn)
-  ; ("trait", KwTrait)
-  ; ("unsafe", KwUnsafe)
-  ; ("val", KwVal)
-  ; ("var", KwVar)
-  ; ("while", KwWhile)
-  ]
+module Make () : S = struct
+  open Basic
 
-let symbol_strings =
-  [
-    ("<-", LtMinus)
-  ; (":=", ColonEq)
-  ; ("!=", BangEq)
-  ; ("<=", LtEq)
-  ; (">=", GtEq)
-  ; ("<<", LtLt)
-  ; (">>", GtGt)
-  ; ("**", StarStar)
-  ; ("|>", PipeGt)
-  ; ("->", MinusGt)
-  ; ("=", Eq)
-  ; ("<", Lt)
-  ; (">", Gt)
-  ; ("+", Plus)
-  ; ("-", Minus)
-  ; ("*", Star)
-  ; ("/", Slash)
-  ; ("&", Amp)
-  ; ("|", Pipe)
-  ; ("^", Caret)
-  ; ("~", Tilde)
-  ; ("@", At)
-  ; ("!", Bang)
-  ; ("$", Dollar)
-  ; ("(", LParen)
-  ; (")", RParen)
-  ; ("{", LBrace)
-  ; ("}", RBrace)
-  ; ("[", LBrack)
-  ; ("]", RBrack)
-  ; (",", Comma)
-  ; (";", Semi)
-  ; (":", Colon)
-  ; (".", Dot)
-  ; ("_", Underscore)
-  ; ("?", Question)
-  ]
+  type t =
+    | LitNumber of string
+    | LitString of Interner.name
+    | LitRune of char
+    | LitTemplate of Interner.name
+    | Ident of Interner.name
+    | KwAnd
+    | KwAs
+    | KwBreak
+    | KwCase
+    | KwChoice
+    | KwCycle
+    | KwDefer
+    | KwElse
+    | KwExport
+    | KwExtern
+    | KwFn
+    | KwFor
+    | KwFrom
+    | KwIf
+    | KwImport
+    | KwIn
+    | KwIs
+    | KwMatch
+    | KwNot
+    | KwOr
+    | KwRecord
+    | KwReturn
+    | KwTrait
+    | KwUnsafe
+    | KwVal
+    | KwVar
+    | KwWhile
+    | LtMinus
+    | ColonEq
+    | Eq
+    | BangEq
+    | Lt
+    | LtEq
+    | Gt
+    | GtEq
+    | Plus
+    | Minus
+    | Star
+    | Slash
+    | StarStar
+    | PipeGt
+    | LtLt
+    | GtGt
+    | Amp
+    | Pipe
+    | Caret
+    | Tilde
+    | At
+    | Bang
+    | Dollar
+    | LParen
+    | RParen
+    | LBrace
+    | RBrace
+    | LBrack
+    | RBrack
+    | Comma
+    | Semi
+    | Colon
+    | Dot
+    | MinusGt
+    | Underscore
+    | Question
+    | EOF
+    | Newline
+    | Whitespace
+    | Comment of string
+    | Error
 
-let base_table_size = 32
+  let keyword_strings =
+    [
+      ("and", KwAnd)
+    ; ("as", KwAs)
+    ; ("break", KwBreak)
+    ; ("case", KwCase)
+    ; ("choice", KwChoice)
+    ; ("cycle", KwCycle)
+    ; ("defer", KwDefer)
+    ; ("else", KwElse)
+    ; ("export", KwExport)
+    ; ("extern", KwExtern)
+    ; ("fn", KwFn)
+    ; ("for", KwFor)
+    ; ("from", KwFrom)
+    ; ("if", KwIf)
+    ; ("import", KwImport)
+    ; ("in", KwIn)
+    ; ("is", KwIs)
+    ; ("match", KwMatch)
+    ; ("not", KwNot)
+    ; ("or", KwOr)
+    ; ("record", KwRecord)
+    ; ("return", KwReturn)
+    ; ("trait", KwTrait)
+    ; ("unsafe", KwUnsafe)
+    ; ("val", KwVal)
+    ; ("var", KwVar)
+    ; ("while", KwWhile)
+    ]
 
-let keyword_to_string =
-  let tbl = Hashtbl.create base_table_size in
-  List.iter (fun (k, v) -> Hashtbl.add tbl v k) keyword_strings;
-  tbl
+  let symbol_strings =
+    [
+      ("<-", LtMinus)
+    ; (":=", ColonEq)
+    ; ("!=", BangEq)
+    ; ("<=", LtEq)
+    ; (">=", GtEq)
+    ; ("<<", LtLt)
+    ; (">>", GtGt)
+    ; ("**", StarStar)
+    ; ("|>", PipeGt)
+    ; ("->", MinusGt)
+    ; ("=", Eq)
+    ; ("<", Lt)
+    ; (">", Gt)
+    ; ("+", Plus)
+    ; ("-", Minus)
+    ; ("*", Star)
+    ; ("/", Slash)
+    ; ("&", Amp)
+    ; ("|", Pipe)
+    ; ("^", Caret)
+    ; ("~", Tilde)
+    ; ("@", At)
+    ; ("!", Bang)
+    ; ("$", Dollar)
+    ; ("(", LParen)
+    ; (")", RParen)
+    ; ("{", LBrace)
+    ; ("}", RBrace)
+    ; ("[", LBrack)
+    ; ("]", RBrack)
+    ; (",", Comma)
+    ; (";", Semi)
+    ; (":", Colon)
+    ; (".", Dot)
+    ; ("_", Underscore)
+    ; ("?", Question)
+    ]
 
-let symbol_to_string =
-  let tbl = Hashtbl.create base_table_size in
-  List.iter (fun (k, v) -> Hashtbl.add tbl v k) symbol_strings;
-  tbl
+  let base_table_size = 32
 
-let to_string = function
-  | LitNumber s -> "NUMBER(" ^ s ^ ")"
-  | LitString name -> "STRING(id:" ^ string_of_int name ^ ")"
-  | LitRune c -> "RUNE(" ^ String.make 1 c ^ ")"
-  | LitTemplate name -> "TEMPLATE(id:" ^ string_of_int name ^ ")"
-  | Ident name -> "IDENT(id:" ^ string_of_int name ^ ")"
-  | ( KwAnd | KwAs | KwBreak | KwCase | KwChoice | KwCycle | KwDefer | KwElse
-    | KwExport | KwExtern | KwFn | KwFor | KwFrom | KwIf | KwImport | KwIn
-    | KwIs | KwMatch | KwNot | KwOr | KwRecord | KwReturn | KwTrait | KwUnsafe
-    | KwVal | KwVar | KwWhile ) as kw ->
-    Hashtbl.find keyword_to_string kw
-  | ( LtMinus | ColonEq | Eq | BangEq | Lt | LtEq | Gt | GtEq | LtLt | GtGt
-    | Plus | Minus | Star | Slash | StarStar | PipeGt | Amp | Pipe | Caret
-    | Tilde | At | Bang | Dollar | LParen | RParen | LBrace | RBrace | LBrack
-    | RBrack | Comma | Semi | Colon | Dot | MinusGt | Underscore | Question ) as
-    sym ->
-    Hashtbl.find symbol_to_string sym
-  | EOF -> "EOF"
-  | Newline -> "NEWLINE"
-  | Whitespace -> "WHITESPACE"
-  | Comment s -> "COMMENT(" ^ s ^ ")"
-  | Error -> "ERROR"
+  let keyword_to_string =
+    let tbl = Hashtbl.create base_table_size in
+    List.iter (fun (k, v) -> Hashtbl.add tbl v k) keyword_strings;
+    tbl
 
-let keywords =
-  let tbl = Hashtbl.create base_table_size in
-  List.iter (fun (k, v) -> Hashtbl.add tbl k v) keyword_strings;
-  tbl
+  let symbol_to_string =
+    let tbl = Hashtbl.create base_table_size in
+    List.iter (fun (k, v) -> Hashtbl.add tbl v k) symbol_strings;
+    tbl
 
-let lookup_keyword interner s =
-  match Hashtbl.find_opt keywords s with
-  | Some kw -> kw
-  | None ->
-    let name = Interner.intern interner s in
-    Ident name
+  let to_string = function
+    | LitNumber s -> "NUMBER(" ^ s ^ ")"
+    | LitString name -> "STRING(id:" ^ string_of_int name ^ ")"
+    | LitRune c -> "RUNE(" ^ String.make 1 c ^ ")"
+    | LitTemplate name -> "TEMPLATE(id:" ^ string_of_int name ^ ")"
+    | Ident name -> "IDENT(id:" ^ string_of_int name ^ ")"
+    | ( KwAnd | KwAs | KwBreak | KwCase | KwChoice | KwCycle | KwDefer | KwElse
+      | KwExport | KwExtern | KwFn | KwFor | KwFrom | KwIf | KwImport | KwIn
+      | KwIs | KwMatch | KwNot | KwOr | KwRecord | KwReturn | KwTrait | KwUnsafe
+      | KwVal | KwVar | KwWhile ) as kw ->
+      Hashtbl.find keyword_to_string kw
+    | ( LtMinus | ColonEq | Eq | BangEq | Lt | LtEq | Gt | GtEq | LtLt | GtGt
+      | Plus | Minus | Star | Slash | StarStar | PipeGt | Amp | Pipe | Caret
+      | Tilde | At | Bang | Dollar | LParen | RParen | LBrace | RBrace | LBrack
+      | RBrack | Comma | Semi | Colon | Dot | MinusGt | Underscore | Question )
+      as sym ->
+      Hashtbl.find symbol_to_string sym
+    | EOF -> "EOF"
+    | Newline -> "NEWLINE"
+    | Whitespace -> "WHITESPACE"
+    | Comment s -> "COMMENT(" ^ s ^ ")"
+    | Error -> "ERROR"
 
-let symbols =
-  let tbl = Hashtbl.create base_table_size in
-  List.iter (fun (k, v) -> Hashtbl.add tbl k v) symbol_strings;
-  tbl
+  let keywords =
+    let tbl = Hashtbl.create base_table_size in
+    List.iter (fun (k, v) -> Hashtbl.add tbl k v) keyword_strings;
+    tbl
+
+  let lookup_keyword interner s =
+    match Hashtbl.find_opt keywords s with
+    | Some kw -> kw
+    | None ->
+      let name = Interner.intern interner s in
+      Ident name
+
+  let symbols =
+    let tbl = Hashtbl.create base_table_size in
+    List.iter (fun (k, v) -> Hashtbl.add tbl k v) symbol_strings;
+    tbl
+end
+
+include Make ()
