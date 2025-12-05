cd0c78f10a89820fea75736632b8a8ffc3761951
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-06 01:51:25 +0200

    stubs

--- lib/parse/parser.ml
+++ lib/parse/parser.ml
@@ -1,5 +1,7 @@
+open Basic
 module Token = Lex.Token
 module Combinator = Combinator
+module Node = Ast.Node
 
 type binding_power = { left : int; right : int }
 
@@ -29,19 +31,43 @@ let can_bind_infix (tok, _) =
     true
   with Not_found -> false
 
-let rec parse_expr bp nud led stream =
-  match nud stream with
-  | Error e -> Error e
-  | Ok (left, stream') -> parse_expr_tail bp nud led left stream'
-
-and parse_expr_tail bp nud led left stream =
-  match Combinator.peek stream with
-  | Ok (tok, _) when can_bind_infix tok ->
-    let bp' = infix_bp (fst tok) in
-    if bp'.left > bp then
-      match led left stream with
-      | Error e -> Error e
-      | Ok (new_left, stream') ->
-        parse_expr_tail bp'.right nud led new_left stream'
-    else Ok (left, stream)
-  | _ -> Ok (left, stream)
+let parse_prog stream = failwith "TODO: implement parse_prog"
+let parse_stmt_list stream = failwith "TODO: implement parse_stmt_list"
+let parse_stmt stream = failwith "TODO: implement parse_stmt"
+let parse_pat stream = failwith "TODO: implement parse_pat"
+let parse_typ stream = failwith "TODO: implement parse_typ"
+let rec parse_expr bp stream = failwith "TODO: implement parse_expr"
+let nud_expr_lit stream = failwith "TODO: implement nud_expr_lit"
+let nud_expr_ident stream = failwith "TODO: implement nud_expr_ident"
+let nud_expr_tuple stream = failwith "TODO: implement nud_expr_tuple"
+let nud_expr_block stream = failwith "TODO: implement nud_expr_block"
+let nud_expr_if stream = failwith "TODO: implement nud_expr_if"
+let nud_expr_match stream = failwith "TODO: implement nud_expr_match"
+let nud_expr_for stream = failwith "TODO: implement nud_expr_for"
+let nud_expr_while stream = failwith "TODO: implement nud_expr_while"
+let nud_expr_record stream = failwith "TODO: implement nud_expr_record"
+let nud_expr_choice stream = failwith "TODO: implement nud_expr_choice"
+let nud_expr_fn stream = failwith "TODO: implement nud_expr_fn"
+let nud_expr_unary stream = failwith "TODO: implement nud_expr_unary"
+let led_expr_call left stream = failwith "TODO: implement led_expr_call"
+let led_expr_member left stream = failwith "TODO: implement led_expr_member"
+let led_expr_index left stream = failwith "TODO: implement led_expr_index"
+let led_expr_assign left stream = failwith "TODO: implement led_expr_assign"
+let led_expr_infix left stream = failwith "TODO: implement led_expr_infix"
+let parse_binding stream = failwith "TODO: implement parse_binding"
+let parse_block stream = failwith "TODO: implement parse_block"
+let parse_guard stream = failwith "TODO: implement parse_guard"
+let parse_import_clause stream = failwith "TODO: implement parse_import_clause"
+let parse_export_clause stream = failwith "TODO: implement parse_export_clause"
+let pat_lit stream = failwith "TODO: implement pat_lit"
+let pat_wild stream = failwith "TODO: implement pat_wild"
+let pat_ident stream = failwith "TODO: implement pat_ident"
+let pat_tuple stream = failwith "TODO: implement pat_tuple"
+let pat_record stream = failwith "TODO: implement pat_record"
+let pat_ctor stream = failwith "TODO: implement pat_ctor"
+let typ_ptr stream = failwith "TODO: implement typ_ptr"
+let typ_array stream = failwith "TODO: implement typ_array"
+let typ_ident stream = failwith "TODO: implement typ_ident"
+let typ_tuple stream = failwith "TODO: implement typ_tuple"
+let typ_fn stream = failwith "TODO: implement typ_fn"
+let typ_record stream = failwith "TODO: implement typ_record"
