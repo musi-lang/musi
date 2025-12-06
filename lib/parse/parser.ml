942a8a1de04f0e62f2728c96f6bc6acf48292130
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-06 08:55:59 +0200

    fix shit

--- lib/parse/parser.ml
+++ lib/parse/parser.ml
@@ -1,55 +1,144 @@
-module Parser = struct
-  open Basic
-  open Combinator
-  module Token = Lex.Token
-  module Node = Ast.Node
+open Basic
+module Token = Lex.Token
+open Ast
 
-  type bp = { lhs : int; rhs : int }
+type state = {
+    tokens : (Token.t * Span.t) list
+  ; mutable pos : int
+  ; len : int
+  ; file_id : Interner.file_id
+  ; _interner : Interner.t
+  ; mutable diags : Diagnostic.bag
+}
 
-  let infix_bp = function
-    | Token.Dot -> { lhs = 90; rhs = 91 }
-    | Token.LBrack -> { lhs = 89; rhs = 90 }
-    | Token.LParen -> { lhs = 88; rhs = 89 }
-    | Token.StarStar -> { lhs = 80; rhs = 81 }
-    | Token.Star | Token.Slash -> { lhs = 70; rhs = 71 }
-    | Token.Plus | Token.Minus -> { lhs = 60; rhs = 61 }
-    | Token.LtLt | Token.GtGt -> { lhs = 50; rhs = 51 }
-    | Token.Eq | Token.BangEq | Token.Lt | Token.LtEq | Token.Gt | Token.GtEq ->
-      { lhs = 40; rhs = 41 }
-    | Token.Amp | Token.KwAnd -> { lhs = 30; rhs = 31 }
-    | Token.Caret -> { lhs = 25; rhs = 26 }
-    | Token.Pipe | Token.KwOr -> { lhs = 20; rhs = 21 }
-    | Token.LtMinus -> { lhs = 0; rhs = 1 }
-    | _ -> raise Not_found
+type 'a t = state -> ('a * state, Diagnostic.t) Result.t
 
-  let prefix_bp = function
-    | Token.Minus | Token.Tilde | Token.KwNot | Token.At -> 100
-    | _ -> raise Not_found
+let mk_state tokens file_id interner =
+  {
+    tokens
+  ; pos = 0
+  ; len = List.length tokens
+  ; file_id
+  ; _interner = interner
+  ; diags = Diagnostic.empty_bag
+  }
 
-  let can_bind_infix tok =
-    try
-      ignore (infix_bp tok);
-      true
-    with Not_found -> false
+let return (x : 'a) : 'a t = fun st -> Ok (x, st)
 
-  let parse_lit =
-    with_span
-      ( token >>= fun (tok, span) ->
-        match tok with
-        | Token.LitNumber s ->
-          ret_ok (Node.Expr.lit ~value:(Ast.Node.Whole s) span)
-        | Token.LitString name ->
-          ret_ok (Node.Expr.lit ~value:(Ast.Node.String name) span)
-        | Token.LitRune c ->
-          ret_ok (Node.Expr.lit ~value:(Ast.Node.Rune c) span)
-        | _ -> parse_error Errors.E1003 span [] )
+let bind (m : 'a t) (f : 'a -> 'b t) : 'b t =
+ fun st -> match m st with Error diag -> Error diag | Ok (x, st') -> f x st'
 
-  let parse_ident =
-    with_span
-      ( token >>= fun (tok, span) ->
-        match tok with
-        | Token.Ident name -> ret_ok (Node.Expr.ident ~value:name span)
-        | _ -> parse_error Errors.E1007 span [] )
+let ( >>= ) = bind
+let ( >>| ) m f = m >>= fun x -> return (f x)
 
-  let parse_atom = choice [ parse_lit; parse_ident ]
-end
+let parse_error err_code span : 'a t =
+ fun _ ->
+  let diag = Errors.parse_diag err_code span [] in
+  Error diag
+
+let catch_error (m : 'a t) (handler : Diagnostic.t -> 'a t) : 'a t =
+ fun st ->
+  match m st with Ok result -> Ok result | Error diag -> handler diag st
+
+let or_else (m1 : 'a t) (m2 : 'a t) : 'a t =
+ fun st -> match m1 st with Ok result -> Ok result | Error _ -> m2 st
+
+let ( <|> ) = or_else
+
+let advance : unit t =
+ fun st ->
+  st.pos <- st.pos + 1;
+  Ok ((), st)
+
+let token : (Token.t * Span.t) t =
+ fun st ->
+  if st.pos >= st.len then
+    parse_error
+      (Errors.E1001 ("unexpected EOF", "expected token"))
+      (Span.make st.file_id st.pos st.pos)
+      st
+  else
+    let tok_span = List.nth st.tokens st.pos in
+    match advance st with
+    | Ok ((), st') -> Ok (tok_span, st')
+    | Error diag -> Error diag
+
+let peek st =
+  if st.pos >= st.len then Token.EOF else fst (List.nth st.tokens st.pos)
+
+let peek_span st =
+  if st.pos >= st.len then Span.dummy else snd (List.nth st.tokens st.pos)
+
+let curr_opt st =
+  if st.pos >= st.len then None else Some (List.nth st.tokens st.pos)
+
+let choice parsers =
+  List.fold_right
+    or_else
+    parsers
+    (parse_error (Errors.E1002 "no matching parser") Span.dummy)
+
+let optional (parser : 'a t) : 'a option t =
+ fun st ->
+  match parser st with
+  | Ok (x, st') -> Ok (Some x, st')
+  | Error _ -> Ok (None, st)
+
+let many (parser : 'a t) : 'a list t =
+ fun st ->
+  let rec collect acc st =
+    match parser st with
+    | Ok (x, st') -> collect (x :: acc) st'
+    | Error _ -> Ok (List.rev acc, st)
+  in
+  collect [] st
+
+let many1 (parser : 'a t) : 'a list t =
+  parser >>= fun first ->
+  many parser >>= fun rest -> return (first :: rest)
+
+let sep_by (parser : 'a t) (sep : 'b t) : 'a list t =
+  parser
+  >>= (fun first ->
+  many (sep >>= fun _ -> parser) >>= fun rest -> return (first :: rest))
+  <|> return []
+
+let between (open_parser : 'a t) (close_parser : 'b t) (content_parser : 'c t) :
+  'c t =
+  open_parser >>= fun _ ->
+  content_parser >>= fun content ->
+  close_parser >>= fun _ -> return content
+
+let token_match (matcher : Token.t -> bool) : Token.t t =
+  token >>= fun (tok, span) ->
+  if matcher tok then return tok else parse_error Errors.E1004 span
+
+let token_expect (expected : Token.t) : unit t =
+  token_match (fun tok -> tok = expected) >>= fun _ -> return ()
+
+let ident_parser : Node.ident t =
+  token_match (function Token.Ident _ -> true | _ -> false) >>= fun tok ->
+  match tok with Token.Ident name -> return name | _ -> assert false
+
+let keyword_parser (kw : Token.t) : unit t =
+  token_match (fun tok -> tok = kw) >>= fun _ -> return ()
+
+let report_error st err_code span =
+  let diag = Errors.parse_diag err_code span [] in
+  st.diags <- Diagnostic.add st.diags diag
+
+let delimited_list (left : Token.t) (right : Token.t) (sep : Token.t)
+  (parser : 'a t) : 'a list t =
+  token_expect left >>= fun () ->
+  parser >>= fun first ->
+  many (token_expect sep >>= fun _ -> parser) >>= fun rest ->
+  token_expect right >>= fun () ->
+  return (first :: rest)
+  <|> ( token_expect left >>= fun () ->
+        token_expect right >>= fun () -> return [] )
+
+let parse_typ : Node.typ t = failwith "TODO: implement parse_typ"
+let parse_pat : Node.pat t = failwith "TODO: implement parse_pat"
+let parse_expr : Node.expr t = failwith "TODO: implement parse_expr"
+let parse_stmt : Node.stmt t = failwith "TODO: implement parse_stmt"
+let parse_prog : Node.prog t = failwith "TODO: implement parse_prog"
--- lib/parse/parser.mli
+++ lib/parse/parser.mli
@@ -1,1 +1,33 @@
-module Parser : module type of Parser
+open Basic
+
+type state
+type 'a t = state -> ('a * state, Diagnostic.t) Result.t
+
+val mk_state :
+  (Lex.Token.t * Span.t) list -> Interner.file_id -> Interner.t -> state
+
+val return : 'a -> 'a t
+val bind : 'a t -> ('a -> 'b t) -> 'b t
+val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+val ( >>| ) : 'a t -> ('a -> 'b) -> 'b t
+val parse_error : Errors.parse_error -> Span.t -> 'a t
+val catch_error : 'a t -> (Diagnostic.t -> 'a t) -> 'a t
+val or_else : 'a t -> 'a t -> 'a t
+val ( <|> ) : 'a t -> 'a t -> 'a t
+val advance : unit t
+val token : (Lex.Token.t * Span.t) t
+val peek : state -> Lex.Token.t
+val peek_span : state -> Span.t
+val curr_opt : state -> (Lex.Token.t * Span.t) option
+val choice : 'a t list -> 'a t
+val optional : 'a t -> 'a option t
+val many : 'a t -> 'a list t
+val many1 : 'a t -> 'a list t
+val sep_by : 'a t -> 'b t -> 'a list t
+val between : 'a t -> 'b t -> 'c t -> 'c t
+val token_match : (Lex.Token.t -> bool) -> Lex.Token.t t
+val token_expect : Lex.Token.t -> unit t
+val ident_parser : Ast.Node.ident t
+val keyword_parser : Lex.Token.t -> unit t
+val report_error : state -> Errors.parse_error -> Span.t -> unit
+val parse_expr : Ast.Node.expr t
