27a4cfabb2200892960e9bd28c6a436249a637e5
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-06 07:13:46 +0200

    refact

--- lib/lex/lexer.mli
+++ lib/lex/lexer.mli
@@ -1,1 +1,22 @@
-module Lexer : module type of Lexer
+open Basic
+
+type state = {
+    source : string
+  ; mutable pos : int
+  ; len : int
+  ; file_id : Interner.file_id
+  ; interner : Interner.t
+  ; mutable diags : Diagnostic.bag
+}
+
+val mk_state : string -> Interner.file_id -> Interner.t -> state
+val peek : state -> char
+val peek_n : state -> int -> char
+val advance : state -> unit
+val advance_n : state -> int -> unit
+
+val tokenize :
+     string
+  -> Interner.file_id
+  -> Interner.t
+  -> (Token.t * Span.t) list * Diagnostic.bag
--- lib/lex/token.ml
+++ lib/lex/token.ml
@@ -1,231 +1,184 @@
-module Token = struct
-  type t =
-    (* Literals *)
-    | LitNumber of string
-    | LitString of int
-    | LitRune of char
-    | LitTemplate of int
-    (* Identifiers *)
-    | Ident of int
-    (* Keywords (alphabetically) *)
-    | KwAnd
-    | KwAs
-    | KwBreak
-    | KwCase
-    | KwChoice
-    | KwCycle
-    | KwDefer
-    | KwElse
-    | KwExport
-    | KwExtern
-    | KwFn
-    | KwFor
-    | KwFrom
-    | KwIf
-    | KwImport
-    | KwIn
-    | KwIs
-    | KwMatch
-    | KwNot
-    | KwOr
-    | KwRecord
-    | KwReturn
-    | KwUnsafe
-    | KwVal
-    | KwVar
-    | KwWhile
-    (* Operators *)
-    | DotDotLt
-    | DotDot
-    | LtMinus
-    | ColonEq
-    | Eq
-    | BangEq
-    | Lt
-    | LtEq
-    | Gt
-    | GtEq
-    | Plus
-    | Minus
-    | Star
-    | Slash
-    | StarStar
-    | PipeGt
-    | LtLt
-    | GtGt
-    | Amp
-    | Pipe
-    | Caret
-    | Tilde
-    | At
-    | Bang
-    (* Punctuation *)
-    | LParen
-    | RParen
-    | LBrace
-    | RBrace
-    | LBrack
-    | RBrack
-    | Comma
-    | Semi
-    | Colon
-    | Dot
-    | MinusGt
-    | EqGt
-    | Underscore
-    | Question
-    (* Special *)
-    | Error
-    | EOF
-    | Newline
-    | Whitespace
-    | Comment of string
 
-  let keyword_strings =
-    [
-      ("and", KwAnd)
-    ; ("as", KwAs)
-    ; ("break", KwBreak)
-    ; ("case", KwCase)
-    ; ("choice", KwChoice)
-    ; ("cycle", KwCycle)
-    ; ("defer", KwDefer)
-    ; ("else", KwElse)
-    ; ("export", KwExport)
-    ; ("extern", KwExtern)
-    ; ("fn", KwFn)
-    ; ("for", KwFor)
-    ; ("from", KwFrom)
-    ; ("if", KwIf)
-    ; ("import", KwImport)
-    ; ("in", KwIn)
-    ; ("is", KwIs)
-    ; ("match", KwMatch)
-    ; ("not", KwNot)
-    ; ("or", KwOr)
-    ; ("record", KwRecord)
-    ; ("return", KwReturn)
-    ; ("unsafe", KwUnsafe)
-    ; ("val", KwVal)
-    ; ("var", KwVar)
-    ; ("while", KwWhile)
-    ]
+type t =
+  | LitNumber of string
+  | LitString of int
+  | LitRune of char
+  | LitTemplate of int
+  | Ident of int
+  | KwAnd
+  | KwAs
+  | KwBreak
+  | KwCase
+  | KwChoice
+  | KwCycle
+  | KwDefer
+  | KwElse
+  | KwExport
+  | KwExtern
+  | KwFn
+  | KwFor
+  | KwFrom
+  | KwIf
+  | KwImport
+  | KwIn
+  | KwIs
+  | KwMatch
+  | KwNot
+  | KwOr
+  | KwRecord
+  | KwReturn
+  | KwUnsafe
+  | KwVal
+  | KwVar
+  | KwWhile
+  | DotDotLt
+  | DotDot
+  | LtMinus
+  | ColonEq
+  | Eq
+  | BangEq
+  | Lt
+  | LtEq
+  | Gt
+  | GtEq
+  | Plus
+  | Minus
+  | Star
+  | Slash
+  | StarStar
+  | PipeGt
+  | LtLt
+  | GtGt
+  | Amp
+  | Pipe
+  | Caret
+  | Tilde
+  | At
+  | Bang
+  | LParen
+  | RParen
+  | LBrace
+  | RBrace
+  | LBrack
+  | RBrack
+  | Comma
+  | Semi
+  | Colon
+  | Dot
+  | MinusGt
+  | EqGt
+  | Underscore
+  | Question
+  | Unknown
+  | EOF
+  | Newline
+  | Whitespace
+  | Comment of string
 
-  let symbol_strings =
-    [
-      ("..<", DotDotLt)
-    ; ("..", DotDot)
-    ; ("<-", LtMinus)
-    ; (":=", ColonEq)
-    ; ("!=", BangEq)
-    ; ("<=", LtEq)
-    ; (">=", GtEq)
-    ; ("<<", LtLt)
-    ; (">>", GtGt)
-    ; ("**", StarStar)
-    ; ("|>", PipeGt)
-    ; ("->", MinusGt)
-    ; ("=>", EqGt)
-    ; ("=", Eq)
-    ; ("<", Lt)
-    ; (">", Gt)
-    ; ("+", Plus)
-    ; ("-", Minus)
-    ; ("*", Star)
-    ; ("/", Slash)
-    ; ("&", Amp)
-    ; ("|", Pipe)
-    ; ("^", Caret)
-    ; ("~", Tilde)
-    ; ("@", At)
-    ; ("!", Bang)
-    ; ("(", LParen)
-    ; (")", RParen)
-    ; ("{", LBrace)
-    ; ("}", RBrace)
-    ; ("[", LBrack)
-    ; ("]", RBrack)
-    ; (",", Comma)
-    ; (";", Semi)
-    ; (":", Colon)
-    ; (".", Dot)
-    ; ("_", Underscore)
-    ; ("?", Question)
-    ]
+let keyword_strings =
+  [
+    ("and", KwAnd)
+  ; ("as", KwAs)
+  ; ("break", KwBreak)
+  ; ("case", KwCase)
+  ; ("choice", KwChoice)
+  ; ("cycle", KwCycle)
+  ; ("defer", KwDefer)
+  ; ("else", KwElse)
+  ; ("export", KwExport)
+  ; ("extern", KwExtern)
+  ; ("fn", KwFn)
+  ; ("for", KwFor)
+  ; ("from", KwFrom)
+  ; ("if", KwIf)
+  ; ("import", KwImport)
+  ; ("in", KwIn)
+  ; ("is", KwIs)
+  ; ("match", KwMatch)
+  ; ("not", KwNot)
+  ; ("or", KwOr)
+  ; ("record", KwRecord)
+  ; ("return", KwReturn)
+  ; ("unsafe", KwUnsafe)
+  ; ("val", KwVal)
+  ; ("var", KwVar)
+  ; ("while", KwWhile)
+  ]
 
-  let base_table_size = 32
+let symbol_strings =
+  [
+    ("..<", DotDotLt)
+  ; ("..", DotDot)
+  ; ("<-", LtMinus)
+  ; (":=", ColonEq)
+  ; ("!=", BangEq)
+  ; ("<=", LtEq)
+  ; (">=", GtEq)
+  ; ("<<", LtLt)
+  ; (">>", GtGt)
+  ; ("**", StarStar)
+  ; ("|>", PipeGt)
+  ; ("->", MinusGt)
+  ; ("=>", EqGt)
+  ; ("=", Eq)
+  ; ("<", Lt)
+  ; (">", Gt)
+  ; ("+", Plus)
+  ; ("-", Minus)
+  ; ("*", Star)
+  ; ("/", Slash)
+  ; ("&", Amp)
+  ; ("|", Pipe)
+  ; ("^", Caret)
+  ; ("~", Tilde)
+  ; ("@", At)
+  ; ("!", Bang)
+  ; ("(", LParen)
+  ; (")", RParen)
+  ; ("{", LBrace)
+  ; ("}", RBrace)
+  ; ("[", LBrack)
+  ; ("]", RBrack)
+  ; (",", Comma)
+  ; (";", Semi)
+  ; (":", Colon)
+  ; (".", Dot)
+  ; ("_", Underscore)
+  ; ("?", Question)
+  ]
 
-  let keyword_to_string =
-    let tbl = Hashtbl.create base_table_size in
-    List.iter (fun (k, v) -> Hashtbl.add tbl v k) keyword_strings;
-    tbl
+let base_table_size = 32
 
-  let symbol_to_string =
-    let tbl = Hashtbl.create base_table_size in
-    List.iter (fun (k, v) -> Hashtbl.add tbl v k) symbol_strings;
-    tbl
+let keyword_to_string =
+  let tbl = Hashtbl.create base_table_size in
+  List.iter (fun (k, v) -> Hashtbl.add tbl v k) keyword_strings;
+  tbl
 
-  let show = function
-    | LitNumber s -> "NUMBER(" ^ s ^ ")"
-    | LitString name -> "STRING(id:" ^ string_of_int name ^ ")"
-    | LitRune c -> "RUNE(" ^ String.make 1 c ^ ")"
-    | LitTemplate name -> "TEMPLATE(id:" ^ string_of_int name ^ ")"
-    | Ident name -> "IDENT(id:" ^ string_of_int name ^ ")"
-    | ( KwAnd | KwAs | KwBreak | KwCase | KwChoice | KwCycle | KwDefer | KwElse
-      | KwExport | KwExtern | KwFn | KwFor | KwFrom | KwIf | KwImport | KwIn
-      | KwIs | KwMatch | KwNot | KwOr | KwRecord | KwReturn | KwUnsafe | KwVal
-      | KwVar | KwWhile ) as kw ->
-      Hashtbl.find keyword_to_string kw
-    | ( DotDotLt | DotDot | LtMinus | ColonEq | Eq | BangEq | Lt | LtEq | Gt
-      | GtEq | LtLt | GtGt | Plus | Minus | Star | Slash | StarStar | PipeGt
-      | Amp | Pipe | Caret | Tilde | At | Bang | LParen | RParen | LBrace
-      | RBrace | LBrack | RBrack | Comma | Semi | Colon | Dot | MinusGt | EqGt
-      | Underscore | Question ) as sym ->
-      Hashtbl.find symbol_to_string sym
-    | Error -> "ERROR"
-    | EOF -> "EOF"
-    | Newline -> "NEWLINE"
-    | Whitespace -> "WHITESPACE"
-    | Comment s -> "COMMENT(" ^ s ^ ")"
+let symbol_to_string =
+  let tbl = Hashtbl.create base_table_size in
+  List.iter (fun (k, v) -> Hashtbl.add tbl v k) symbol_strings;
+  tbl
 
-  let keywords =
-    let tbl = Hashtbl.create base_table_size in
-    List.iter (fun (k, v) -> Hashtbl.add tbl k v) keyword_strings;
-    tbl
-
-  let lookup_keyword interner s =
-    match Hashtbl.find_opt keywords s with
-    | Some kw -> kw
-    | None ->
-      let name = Basic.Interner.intern interner s in
-      Ident name
-
-  let is_keyword = function
-    | KwAnd | KwAs | KwBreak | KwCase | KwChoice | KwCycle | KwDefer | KwElse
+let show = function
+  | LitNumber s -> "NUMBER(" ^ s ^ ")"
+  | LitString name -> "STRING(id:" ^ string_of_int name ^ ")"
+  | LitRune c -> "RUNE(" ^ String.make 1 c ^ ")"
+  | LitTemplate name -> "TEMPLATE(id:" ^ string_of_int name ^ ")"
+  | Ident name -> "IDENT(id:" ^ string_of_int name ^ ")"
+  | ( KwAnd | KwAs | KwBreak | KwCase | KwChoice | KwCycle | KwDefer | KwElse
     | KwExport | KwExtern | KwFn | KwFor | KwFrom | KwIf | KwImport | KwIn
     | KwIs | KwMatch | KwNot | KwOr | KwRecord | KwReturn | KwUnsafe | KwVal
-    | KwVar | KwWhile ->
-      true
-    | _ -> false
+    | KwVar | KwWhile ) as kw ->
+    Hashtbl.find keyword_to_string kw
+  | ( DotDotLt | DotDot | LtMinus | ColonEq | Eq | BangEq | Lt | LtEq | Gt
+    | GtEq | LtLt | GtGt | Plus | Minus | Star | Slash | StarStar | PipeGt | Amp
+    | Pipe | Caret | Tilde | At | Bang | LParen | RParen | LBrace | RBrace
+    | LBrack | RBrack | Comma | Semi | Colon | Dot | MinusGt | EqGt | Underscore
+    | Question ) as sym ->
+    Hashtbl.find symbol_to_string sym
+  | Unknown -> "UNKNOWN"
+  | EOF -> "EOF"
+  | Newline -> "NEWLINE"
+  | Whitespace -> "WHITESPACE"
+  | Comment s -> "COMMENT(" ^ s ^ ")"
 
-  let is_literal = function
-    | LitNumber _ | LitString _ | LitRune _ | LitTemplate _ -> true
-    | _ -> false
-
-  let is_operator = function
-    | DotDotLt | DotDot | LtMinus | ColonEq | Eq | BangEq | Lt | LtEq | Gt
-    | GtEq | Plus | Minus | Star | Slash | StarStar | PipeGt | LtLt | GtGt | Amp
-    | Pipe | Caret | Tilde | At | Bang ->
-      true
-    | _ -> false
-
-  let is_punctuation = function
-    | LParen | RParen | LBrace | RBrace | LBrack | RBrack | Comma | Semi | Colon
-    | Dot | MinusGt | EqGt | Underscore | Question ->
-      true
-    | _ -> false
-
-  let is_special = function
-    | Error | EOF | Newline | Whitespace | Comment _ -> true
-    | _ -> false
-end
