96d3c24884a60ddd5b4d5acdee99f50934eb6bc8
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-06 07:31:21 +0200

    fix

--- lib/ast/node.ml
+++ lib/ast/node.ml
@@ -1,364 +1,127 @@
-module Node = struct
-  open Basic
-  module Token = Lex.Token
+open Basic
+module Token = Lex.Token
 
-  type ident = Interner.name
-  type attr_arg = Ident of ident | Number of string | String of ident
+type ident = Interner.name
+type attr_arg = Ident of ident | Number of string | String of ident
 
-  type lit =
-    | Whole of string
-    | Float of string
-    | String of ident
-    | Rune of char
+type lit =
+  | LitWhole of string
+  | LitFloat of string
+  | LitString of ident
+  | LitRune of char
 
-  type import_clause = All of ident | Named of ident list
-  type export_clause = All of ident | Named of ident list
-  type stmt = [ `Stmt ]
-  type expr = [ `Expr ]
-  type pat = [ `Pat ]
-  type typ = [ `Typ ]
+type import_clause = ImportAll of ident | ImportNamed of ident list
+type export_clause = ExportAll of ident | ExportNamed of ident list
 
-  type 'a node = { span : Span.t; data : 'a node_data }
+type 'a node = { span : Span.t; data : 'a }
+and typ = typ_kind node
+and pat = pat_kind node
+and expr = expr_kind node
+and stmt = stmt_kind node
 
-  and 'a node_data =
-    | Stmt_data : stmt_data -> stmt node_data
-    | Expr_data : expr_data -> expr node_data
-    | Pat_data : pat_data -> pat node_data
-    | Typ_data : typ_data -> typ node_data
+and binding = {
+    mutable_ : bool (* "val" | "var" *)
+  ; name : ident
+  ; typ_annot : typ option
+  ; span : Span.t
+}
 
-  and binding = {
-      mutable_ : bool (* "val" | "var" *)
-    ; name : ident
-    ; typ_annot : typ node option
-    ; span : Span.t
-  }
+and attr = { name : ident; args : attr_arg list; span : Span.t }
+and fn_param = { name : ident; typ_annot : typ option; span : Span.t }
+and fn_params = fn_param list
+and fn_sig = { params : fn_params; ret_typ : typ option; span : Span.t }
+and fn_sig_decl = { name : ident; sig_ : fn_sig; span : Span.t }
+and typ_params = ident list
+and typ_args = typ list
+and field_decl = { name : ident; typ_annot : typ; span : Span.t }
+and field_init = { name : ident; value : expr; shorthand : bool; span : Span.t }
+and field_pat = { name : ident; pat : pat option; span : Span.t }
+and tmpl_string = { parts : tmpl_part list; span : Span.t }
+and tmpl_part = TmplText of ident | TmplExpr of expr
+and cond = CondCaseBinding of { pat : pat; expr : expr } | CondExpr of expr
+and for_binding = ForIdent of ident | ForPat of pat
+and guard = expr option
+and match_arm = { pat : pat; guard : guard; body : expr; span : Span.t }
+and choice_case = { name : ident; fields : typ list; span : Span.t }
+and block = { stmts : stmt list; ret : expr option; span : Span.t }
 
-  and attr = { name : ident; args : attr_arg list; span : Span.t }
-  and fn_param = { name : ident; typ_annot : typ node option; span : Span.t }
-  and fn_params = fn_param list
-  and fn_sig = { params : fn_params; ret_typ : typ node option; span : Span.t }
-  and fn_sig_decl = { name : ident; sig_ : fn_sig; span : Span.t }
-  and typ_params = ident list
-  and typ_args = typ node list
-  and field_decl = { name : ident; typ_annot : typ node; span : Span.t }
+and typ_kind =
+  | TypPtr of typ
+  | TypArray of { size : expr option; elem : typ }
+  | TypIdent of ident
+  | TypApp of { base : ident; args : typ_args }
+  | TypTuple of typ list
+  | TypFn of { params : typ list; ret : typ option }
+  | TypRecord of field_decl list
+  | TypOptional of typ
 
-  and field_init = {
-      name : ident
-    ; value : expr node
-    ; shorthand : bool
-    ; span : Span.t
-  }
+and pat_kind =
+  | PatBind of binding
+  | PatLit of lit
+  | PatWild
+  | PatIdent of ident
+  | PatRecord of { name : ident; fields : field_pat list }
+  | PatCtor of { name : ident; args : pat list }
+  | PatTuple of pat list
 
-  and field_pat = { name : ident; pat : pat node option; span : Span.t }
-  and tmpl_string = { parts : tmpl_part list; span : Span.t }
-  and tmpl_part = Text of ident | Expr of expr node
+and expr_kind =
+  | ExprLit of lit
+  | ExprTmpl of tmpl_string
+  | ExprIdent of ident
+  | ExprTuple of expr list
+  | ExprBlock of block
+  | ExprRange of { start : expr option; end_ : expr option; exclusive : bool }
+  | ExprIf of {
+        conds : (pat * expr) list
+      ; then_block : block
+      ; else_block : block option
+    }
+  | ExprMatch of { scrutinee : expr; arms : match_arm list }
+  | ExprFor of {
+        binding : for_binding
+      ; range : expr
+      ; guard : guard
+      ; body : block
+    }
+  | ExprWhile of { cond : cond option; guard : guard; body : block }
+  | ExprDefer of expr
+  | ExprBreak of expr option
+  | ExprCycle
+  | ExprReturn of expr option
+  | ExprUnsafe of block
+  | ExprAssign of { target : ident; value : expr }
+  | ExprUnary of { op : Token.t; arg : expr }
+  | ExprCall of {
+        callee : expr
+      ; typ_args : typ_args option
+      ; args : expr list
+      ; optional : bool
+    }
+  | ExprMember of { obj : expr; prop : ident; computed : bool; optional : bool }
+  | ExprRecordLit of { name : ident option; fields : field_init list }
+  | ExprFn of {
+        abi : string option
+      ; name : ident option
+      ; typ_params : typ_params option
+      ; sig_ : fn_sig
+      ; body : block
+    }
+  | ExprRecord of {
+        typ_params : typ_params option
+      ; typ_annot : typ option
+      ; fields : field_decl list
+    }
+  | ExprChoice of { typ_params : typ_params option; cases : choice_case list }
 
-  and cond =
-    | CaseBinding of { pat : pat node; expr : expr node }
-    | Expr of expr node
+and stmt_kind =
+  | StmtImport of { clause : import_clause; source : ident }
+  | StmtExport of { clause : export_clause; source : ident option }
+  | StmtBind of { binding : binding; value : expr; attr : attr option }
+  | StmtExtern of { abi : string option; sig_decls : fn_sig_decl list }
+  | StmtExpr of expr
 
-  and for_binding = ForIdent of ident | ForPat of pat node
-  and guard = expr node option
+type prog = stmt list
 
-  and match_arm = {
-      pat : pat node
-    ; guard : guard
-    ; body : expr node
-    ; span : Span.t
-  }
-
-  and choice_case = { name : ident; fields : typ node list; span : Span.t }
-  and block = { stmts : stmt node list; ret : expr node option; span : Span.t }
-
-  and stmt_data =
-    | Import of { clause : import_clause; source : ident }
-    | Export of { clause : export_clause; source : ident option }
-    | Bind of { binding : binding; value : expr node; attr : attr option }
-    | Extern of { abi : string option; sig_decls : fn_sig_decl list }
-    | Expr of expr node
-
-  and expr_data =
-    | Lit of lit
-    | Tmpl of tmpl_string
-    | Ident of ident
-    | Tuple of expr node list
-    | Block of block
-    | Range of {
-          start : expr node option
-        ; end_ : expr node option
-        ; exclusive : bool
-      }
-    | If of {
-          conds : (pat node * expr node) list
-        ; then_block : block
-        ; else_block : block option
-      }
-    | Match of { scrutinee : expr node; arms : match_arm list }
-    | For of {
-          binding : for_binding
-        ; range : expr node
-        ; guard : guard
-        ; body : block
-      }
-    | While of { cond : cond option; guard : guard; body : block }
-    | Defer of expr node
-    | Break of expr node option
-    | Cycle
-    | Return of expr node option
-    | Unsafe of block
-    | Assign of { target : ident; value : expr node }
-    | Unary of { op : Token.t; arg : expr node }
-    | Call of {
-          callee : expr node
-        ; typ_args : typ_args option
-        ; args : expr node list
-        ; optional : bool
-      }
-    | Member of {
-          obj : expr node
-        ; prop : ident
-        ; computed : bool
-        ; optional : bool
-      }
-    | RecordLit of { name : ident option; fields : field_init list }
-    | Fn of {
-          abi : string option
-        ; name : ident option
-        ; typ_params : typ_params option
-        ; sig_ : fn_sig
-        ; body : block
-      }
-    | Record of {
-          typ_params : typ_params option
-        ; typ_annot : typ node option
-        ; fields : field_decl list
-      }
-    | Choice of { typ_params : typ_params option; cases : choice_case list }
-
-  and pat_data =
-    | Bind of binding
-    | Lit of lit
-    | Wild
-    | Ident of ident
-    | Record of { name : ident; fields : field_pat list }
-    | Ctor of { name : ident; args : pat node list }
-    | Tuple of pat node list
-
-  and typ_data =
-    | Ptr of typ node
-    | Array of { size : expr node option; elem : typ node }
-    | Ident of ident
-    | App of { base : ident; args : typ_args }
-    | Tuple of typ node list
-    | Fn of { params : typ node list; ret : typ node option }
-    | Record of field_decl list
-    | Optional of typ node
-
-  type prog = stmt node list
-
-  let span { span; _ } = span
-  let update_span span node = { node with span }
-
-  let map f { span; data } =
-    ignore f;
-    { span; data }
-
-  module Stmt = struct
-    type t = stmt node
-
-    type data = stmt_data =
-      | Import of { clause : import_clause; source : ident }
-      | Export of { clause : export_clause; source : ident option }
-      | Bind of { binding : binding; value : expr node; attr : attr option }
-      | Extern of { abi : string option; sig_decls : fn_sig_decl list }
-      | Expr of expr node
-
-    let import ~clause ~source span =
-      { span; data = Stmt_data (Import { clause; source }) }
-
-    let export ~clause ?source span =
-      { span; data = Stmt_data (Export { clause; source }) }
-
-    let bind ~binding ~value ?attr span =
-      { span; data = Stmt_data (Bind { binding; value; attr }) }
-
-    let extern ?abi ~sig_decls span =
-      { span; data = Stmt_data (Extern { abi; sig_decls }) }
-
-    let expr ~node span = { span; data = Stmt_data (Expr node) }
-    let data { data; _ } = match data with Stmt_data d -> d
-    let span = span
-  end
-
-  module Expr = struct
-    type t = expr node
-
-    type data = expr_data =
-      | Lit of lit
-      | Tmpl of tmpl_string
-      | Ident of ident
-      | Tuple of expr node list
-      | Block of block
-      | Range of {
-            start : expr node option
-          ; end_ : expr node option
-          ; exclusive : bool
-        }
-      | If of {
-            conds : (pat node * expr node) list
-          ; then_block : block
-          ; else_block : block option
-        }
-      | Match of { scrutinee : expr node; arms : match_arm list }
-      | For of {
-            binding : for_binding
-          ; range : expr node
-          ; guard : guard
-          ; body : block
-        }
-      | While of { cond : cond option; guard : guard; body : block }
-      | Defer of expr node
-      | Break of expr node option
-      | Cycle
-      | Return of expr node option
-      | Unsafe of block
-      | Assign of { target : ident; value : expr node }
-      | Unary of { op : Token.t; arg : expr node }
-      | Call of {
-            callee : expr node
-          ; typ_args : typ_args option
-          ; args : expr node list
-          ; optional : bool
-        }
-      | Member of {
-            obj : expr node
-          ; prop : ident
-          ; computed : bool
-          ; optional : bool
-        }
-      | RecordLit of { name : ident option; fields : field_init list }
-      | Fn of {
-            abi : string option
-          ; name : ident option
-          ; typ_params : typ_params option
-          ; sig_ : fn_sig
-          ; body : block
-        }
-      | Record of {
-            typ_params : typ_params option
-          ; typ_annot : typ node option
-          ; fields : field_decl list
-        }
-      | Choice of { typ_params : typ_params option; cases : choice_case list }
-
-    let lit ~value span = { span; data = Expr_data (Lit value) }
-    let tmpl ~value span = { span; data = Expr_data (Tmpl value) }
-    let ident ~value span = { span; data = Expr_data (Ident value) }
-    let tuple ~values span = { span; data = Expr_data (Tuple values) }
-    let block ~value span = { span; data = Expr_data (Block value) }
-
-    let range ?start ?end_ ~exclusive span =
-      { span; data = Expr_data (Range { start; end_; exclusive }) }
-
-    let if_ ~conds ~then_block ?else_block span =
-      { span; data = Expr_data (If { conds; then_block; else_block }) }
-
-    let match_ ~scrutinee ~arms span =
-      { span; data = Expr_data (Match { scrutinee; arms }) }
-
-    let for_ ~binding ~range ?guard ~body span =
-      { span; data = Expr_data (For { binding; range; guard; body }) }
-
-    let while_ ?cond ?guard ~body span =
-      { span; data = Expr_data (While { cond; guard; body }) }
-
-    let defer ~value span = { span; data = Expr_data (Defer value) }
-    let break ?value span = { span; data = Expr_data (Break value) }
-    let cycle span = { span; data = Expr_data Cycle }
-    let return_ ?value span = { span; data = Expr_data (Return value) }
-    let unsafe ~value span = { span; data = Expr_data (Unsafe value) }
-
-    let assign ~target ~value span =
-      { span; data = Expr_data (Assign { target; value }) }
-
-    let unary ~op ~arg span = { span; data = Expr_data (Unary { op; arg }) }
-
-    let call ~callee ?typ_args ~args ?optional span =
-      let optional = Option.value optional ~default:false in
-      { span; data = Expr_data (Call { callee; typ_args; args; optional }) }
-
-    let member ~obj ~prop ?computed ?optional span =
-      let computed = Option.value computed ~default:false in
-      let optional = Option.value optional ~default:false in
-      { span; data = Expr_data (Member { obj; prop; computed; optional }) }
-
-    let record_lit ?name ~fields span =
-      { span; data = Expr_data (RecordLit { name; fields }) }
-
-    let fn ?abi ?name ?typ_params ~sig_ ~body span =
-      { span; data = Expr_data (Fn { abi; name; typ_params; sig_; body }) }
-
-    let record ?typ_params ?typ_annot ~fields span =
-      { span; data = Expr_data (Record { typ_params; typ_annot; fields }) }
-
-    let choice ?typ_params ~cases span =
-      { span; data = Expr_data (Choice { typ_params; cases }) }
-
-    let data { data; _ } = match data with Expr_data d -> d
-    let span = span
-  end
-
-  module Pat = struct
-    type t = pat node
-
-    type data = pat_data =
-      | Bind of binding
-      | Lit of lit
-      | Wild
-      | Ident of ident
-      | Record of { name : ident; fields : field_pat list }
-      | Ctor of { name : ident; args : pat node list }
-      | Tuple of pat node list
-
-    let bind ~binding span = { span; data = Pat_data (Bind binding) }
-    let lit ~value span = { span; data = Pat_data (Lit value) }
-    let wild span = { span; data = Pat_data Wild }
-    let ident ~value span = { span; data = Pat_data (Ident value) }
-
-    let record ~name ~fields span =
-      { span; data = Pat_data (Record { name; fields }) }
-
-    let ctor ~name ~args span = { span; data = Pat_data (Ctor { name; args }) }
-    let tuple ~values span = { span; data = Pat_data (Tuple values) }
-    let data { data; _ } = match data with Pat_data d -> d
-    let span = span
-  end
-
-  module Typ = struct
-    type t = typ node
-
-    type data = typ_data =
-      | Ptr of typ node
-      | Array of { size : expr node option; elem : typ node }
-      | Ident of ident
-      | App of { base : ident; args : typ_args }
-      | Tuple of typ node list
-      | Fn of { params : typ node list; ret : typ node option }
-      | Record of field_decl list
-      | Optional of typ node
-
-    let ptr ~value span = { span; data = Typ_data (Ptr value) }
-
-    let array ?size ~elem span =
-      { span; data = Typ_data (Array { size; elem }) }
-
-    let ident ~value span = { span; data = Typ_data (Ident value) }
-    let app ~base ~args span = { span; data = Typ_data (App { base; args }) }
-    let tuple ~values span = { span; data = Typ_data (Tuple values) }
-    let fn_ ~params ?ret span = { span; data = Typ_data (Fn { params; ret }) }
-    let record ~fields span = { span; data = Typ_data (Record fields) }
-    let optional ~value span = { span; data = Typ_data (Optional value) }
-    let data { data; _ } = match data with Typ_data d -> d
-    let span = span
-  end
-end
+let span { span; _ } = span
+let update_span span node = { node with span }
+let map f { span; data } = { span; data = f data }
