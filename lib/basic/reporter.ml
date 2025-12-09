24e3f09d76a523fc651a5d621fd833082443f658
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-09 17:42:26 +0200

    remove old

--- lib/basic/reporter.ml
+++ lib/basic/reporter.ml
@@ -1,16 +1,8 @@
 type severity = Error | Warning | Note
 type category = Lex | Parse | Sema | Codegen | Runtime
 
-type error_code =
-  | Lex of string
-  | Parse of string
-  | Sema of string
-  | Codegen of string
-  | Runtime of string
-
 type t = {
     severity : severity
-  ; code : error_code option
   ; message : string
   ; span : Span.t
   ; notes : (string * Span.t) list
@@ -18,21 +10,10 @@ type t = {
 
 type bag = { diags : t list; errors : int; warnings : int }
 
-let make severity message span =
-  { severity; code = None; message; span; notes = [] }
-
-let make_with_code severity code message span =
-  { severity; code = Some code; message; span; notes = [] }
-
+let make severity message span = { severity; message; span; notes = [] }
 let error message span = make Error message span
 let warning message span = make Warning message span
 let note message span = make Note message span
-let error_with_code code message span = make_with_code Error code message span
-
-let warning_with_code code message span =
-  make_with_code Warning code message span
-
-let note_with_code code message span = make_with_code Note code message span
 
 let add_note message span diag =
   { diag with notes = (message, span) :: diag.notes }
--- lib/basic/reporter.mli
+++ lib/basic/reporter.mli
@@ -1,21 +1,7 @@
 type severity = Error | Warning | Note
-type category = Lex | Parse | Sema | Codegen | Runtime
-
-type error_code =
-  (* E0001..E0999 *)
-  | Lex of string
-  (* E1001..E1999 *)
-  | Parse of string
-  (* E2001..E2999 *)
-  | Sema of string
-  (* E3001..E3999 *)
-  | Codegen of string
-  (* E4001..E4999 *)
-  | Runtime of string
 
 type t = {
     severity : severity
-  ; code : error_code option
   ; message : string
   ; span : Span.t
   ; notes : (string * Span.t) list
@@ -24,13 +10,10 @@ type t = {
 type bag = { diags : t list; errors : int; warnings : int }
 
 val make : severity -> string -> Span.t -> t
-val make_with_code : severity -> error_code -> string -> Span.t -> t
+val make_with_code : severity -> string -> Span.t -> t
 val error : string -> Span.t -> t
 val warning : string -> Span.t -> t
 val note : string -> Span.t -> t
-val error_with_code : error_code -> string -> Span.t -> t
-val warning_with_code : error_code -> string -> Span.t -> t
-val note_with_code : error_code -> string -> Span.t -> t
 val add_note : string -> Span.t -> t -> t
 val empty_bag : bag
 val add : bag -> t -> bag
