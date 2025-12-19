aa05faee8c0734e7b1ae173d00b1fd1547bc4bcf
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-19 23:11:33 +0200

    nodes

--- lib/ast/nodes.ml
+++ lib/ast/nodes.ml
@@ -1,32 +1,36 @@
 open Basic
 open Lex
 
-type ident = string
-type span = Span.t
-type 'a with_span = { kind : 'a; span : span }
+type 'a with_span = { kind : 'a; span : Span.t }
 
 type modifier = {
     is_export : bool
-  ; is_extern : (string option * bool) option (* Some(path, unsafe) or None *)
+  ; is_extern : (string option * bool) option
   ; is_unsafe : bool
 }
 
-type lit =
-  | LitInt of string
-  | LitFloat of string
-  | LitString of string
-  | LitTemplate of template_part list
+type attr_arg =
+  | AttrArgNamed of ident * ident * lit
+  | AttrArgPos of ident * lit
+
+and attr = { attr_name : ident; attr_args : attr_arg list }
+
+and lit =
+  | LitInt of ident
+  | LitFloat of ident
+  | LitString of ident
   | LitRune of char
   | LitBool of bool
 
-and template_part = TemplateText of string | TemplateExpr of expr
-and ty = ty_kind with_span
+and ident = string
+
+type ty = ty_kind with_span
 
 and ty_kind =
   | TyIdent of ident
   | TyApp of ident * ty list
-  | TyOptional of ty
   | TyArray of int option * ty
+  | TyOptional of ty
   | TyPtr of ty
   | TyFn of ty * ty
   | TyTuple of ty list
@@ -36,8 +40,8 @@ and pat = pat_kind with_span
 
 and pat_kind =
   | PatIdent of ident
-  | PatLit of lit
   | PatWild
+  | PatLit of lit
   | PatLitTuple of pat list
   | PatLitArray of pat list
   | PatLitRecord of ident * pat_field list
@@ -50,33 +54,31 @@ and expr = expr_kind with_span
 
 and expr_kind =
   | ExprLit of lit
-  | ExprIdent of ident
   | ExprLitTuple of expr list
   | ExprLitArray of expr list
-  | ExprLitRecord of ident option * record_content
+  | ExprLitRecord of ident list
+  | ExprIdent of ident
   | ExprBlock of stmt list * expr option
   | ExprIf of expr * expr * expr
   | ExprWhile of expr * expr
   | ExprFor of ident * expr * expr
   | ExprMatch of expr * match_case list
-  | ExprTry of expr * (ident option * expr) option
   | ExprReturn of expr option
-  | ExprDefer of expr
   | ExprBreak of expr option
-  | ExprCycle
+  | ExprDefer of expr
   | ExprUnsafe of expr
   | ExprImport of string
   | ExprExtern of string option * bool * fn_sig list
-  | ExprRecord of
-      attr list * modifier * ident option * ty_params * record_field list
-  | ExprSum of attr list * modifier * ident option * ty_params * sum_case list
-  | ExprFn of attr list * modifier * fn_sig * expr
   | ExprBind of modifier * bool * ident * ty option * expr * expr
+  | ExprFn of attr list * modifier * fn_sig * expr
+  | ExprRecord of
+      attr list * modifier * ident option * ident list * record_field list
+  | ExprSum of attr list * modifier * ident option * ident list * sum_case list
   | ExprCall of expr * expr list
   | ExprIndex of expr * expr
   | ExprField of expr * ident
-  | ExprUnaryPostfix of expr * Token.t
   | ExprUnaryPrefix of Token.t * expr
+  | ExprUnaryPostfix of expr * Token.t
   | ExprBinary of expr * Token.t * expr
   | ExprRange of expr * Token.t * expr option
   | ExprAssign of expr * expr
@@ -86,22 +88,9 @@ and stmt = stmt_kind with_span
 and stmt_kind = StmtExpr of expr
 and match_case = { case_pat : pat; case_expr : expr }
 
-and record_content =
-  | RecordFields of record_field list
-  | RecordWith of expr * record_field list
-
-and record_field = {
-    field_mutable : bool
-  ; field_name : ident
-  ; field_ty : ty option
-  ; field_default : expr option
-}
-
-and sum_case = { case_name : ident; case_tys : ty list; case_params : ty list }
-
 and fn_sig = {
     fn_name : ident option
-  ; fn_ty_params : ty_params
+  ; fn_ty_params : ident list
   ; fn_params : param list
   ; fn_ret_ty : ty option
 }
@@ -113,8 +102,20 @@ and param = {
   ; param_default : expr option
 }
 
-and ty_params = ident list
-and attr = { attr_name : ident; attr_args : attr_arg list }
-and attr_arg = AttrArgPos of ident * lit | AttrArgNamed of ident * ident * lit
+and record_field = {
+    field_mutable : bool
+  ; field_name : ident
+  ; field_ty : ty option
+  ; field_default : expr option
+}
 
-type prog = stmt list
+and sum_case = {
+    case_name : ident
+  ; case_tys : ty list
+  ; case_params : param list
+}
+
+let make_ty kind span = { kind; span }
+let make_pat kind span = { kind; span }
+let make_expr kind span = { kind; span }
+let make_stmt kind span = { kind; span }
