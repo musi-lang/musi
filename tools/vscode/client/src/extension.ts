403807ec4ce6f68cc7c3df727be37044dbbb702a
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-20 21:03:27 +0200

    fix?

--- tools/vscode/client/src/extension.ts
+++ tools/vscode/client/src/extension.ts
@@ -2,7 +2,7 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import * as vscode from "vscode";
 import { window, workspace } from "vscode";
-import { LanguageClient, TransportKind } from "vscode-languageclient/node.js";
+import { LanguageClient, TransportKind } from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
