be559dfa765f210117387df872ee4fb336961a0f
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-20 12:43:47 +0200

    all pass!

--- test/parse_test.ml
+++ test/parse_test.ml
@@ -163,11 +163,8 @@ let mk_ident i = mk_expr (ExprIdent i)
 let mk_ty_ident i = make_ty (TyIdent i) Span.dummy
 let mk_mod = { is_export = false; is_extern = (None, false); is_unsafe = false }
 
-let test_lits () =
+let test_lit_basic () =
   assert_stmt "1024;" (mk_stmt_expr (mk_lit (LitInt "1024")));
-  assert_stmt "0xDEADBEEF;" (mk_stmt_expr (mk_lit (LitInt "0xDEADBEEF")));
-  assert_stmt "0o755;" (mk_stmt_expr (mk_lit (LitInt "0o755")));
-  assert_stmt "0b1010;" (mk_stmt_expr (mk_lit (LitInt "0b1010")));
   assert_stmt "3.14159;" (mk_stmt_expr (mk_lit (LitFloat "3.14159")));
   assert_stmt "true;" (mk_stmt_expr (mk_lit (LitBool true)));
   assert_stmt "false;" (mk_stmt_expr (mk_lit (LitBool false)));
@@ -176,63 +173,76 @@ let test_lits () =
     (mk_stmt_expr (mk_lit (LitString "hello world")));
   assert_stmt "'a';" (mk_stmt_expr (mk_lit (LitRune 'a')))
 
-let test_types () =
-  let assert_ty input expected =
-    let wrapped = Printf.sprintf "var x: %s := 0;" input in
-    let expected_stmt =
-      mk_stmt_expr
-        (mk_expr
-           (ExprBind
-              ( mk_mod
-              , true
-              , "x"
-              , Some expected
-              , mk_lit (LitInt "0")
-              , mk_expr (ExprLit (LitInt "0")) )))
-    in
-    assert_stmt wrapped expected_stmt
+let test_lit_numbers () =
+  assert_stmt "0xDEADBEEF;" (mk_stmt_expr (mk_lit (LitInt "0xDEADBEEF")));
+  assert_stmt "0o755;" (mk_stmt_expr (mk_lit (LitInt "0o755")));
+  assert_stmt "0b1010;" (mk_stmt_expr (mk_lit (LitInt "0b1010")))
+
+let verify_ty input expected =
+  let wrapped = Printf.sprintf "var x: %s := 0;" input in
+  let expected_stmt =
+    mk_stmt_expr
+      (mk_expr
+         (ExprBind
+            ( mk_mod
+            , true
+            , "x"
+            , Some expected
+            , mk_lit (LitInt "0")
+            , mk_expr (ExprLit (LitInt "0")) )))
   in
-  assert_ty "Int" (mk_ty_ident "Int");
-  assert_ty
+  assert_stmt wrapped expected_stmt
+
+let test_ty_basic () =
+  verify_ty "Int" (mk_ty_ident "Int");
+  verify_ty
     "List<Int>"
-    (make_ty (TyApp ("List", [ mk_ty_ident "Int" ])) Span.dummy);
-  assert_ty "?Int" (make_ty (TyOptional (mk_ty_ident "Int")) Span.dummy);
-  assert_ty "^Int" (make_ty (TyPtr (mk_ty_ident "Int")) Span.dummy);
-  assert_ty "[]Int" (make_ty (TyArray (None, mk_ty_ident "Int")) Span.dummy);
-  assert_ty
+    (make_ty (TyApp ("List", [ mk_ty_ident "Int" ])) Span.dummy)
+
+let test_ty_ptr_opt () =
+  verify_ty "?Int" (make_ty (TyOptional (mk_ty_ident "Int")) Span.dummy);
+  verify_ty "^Int" (make_ty (TyPtr (mk_ty_ident "Int")) Span.dummy)
+
+let test_ty_struct () =
+  verify_ty "[]Int" (make_ty (TyArray (None, mk_ty_ident "Int")) Span.dummy);
+  verify_ty
     "[10]Int"
     (make_ty (TyArray (Some 10, mk_ty_ident "Int")) Span.dummy);
-  assert_ty
+  verify_ty
+    "(Int, String)"
+    (make_ty (TyTuple [ mk_ty_ident "Int"; mk_ty_ident "String" ]) Span.dummy)
+
+let test_ty_fn () =
+  verify_ty
     "Int -> String"
     (make_ty (TyFn (mk_ty_ident "Int", mk_ty_ident "String")) Span.dummy);
-  assert_ty
+  verify_ty
     "Int -> String -> Bool"
     (make_ty
        (TyFn
           ( mk_ty_ident "Int"
           , make_ty (TyFn (mk_ty_ident "String", mk_ty_ident "Bool")) Span.dummy
           ))
-       Span.dummy);
-  assert_ty
-    "(Int, String)"
-    (make_ty (TyTuple [ mk_ty_ident "Int"; mk_ty_ident "String" ]) Span.dummy)
+       Span.dummy)
 
-let test_pats () =
-  let assert_pat input expected =
-    let wrapped = Printf.sprintf "match x { case %s => 0 };" input in
-    let expected_stmt =
-      mk_stmt_expr
-        (mk_expr
-           (ExprMatch
-              ( mk_ident "x"
-              , [ { case_pat = expected; case_expr = mk_lit (LitInt "0") } ] )))
-    in
-    assert_stmt wrapped expected_stmt
+let verify_pat input expected =
+  let wrapped = Printf.sprintf "match x { case %s => 0 };" input in
+  let expected_stmt =
+    mk_stmt_expr
+      (mk_expr
+         (ExprMatch
+            ( mk_ident "x"
+            , [ { case_pat = expected; case_expr = mk_lit (LitInt "0") } ] )))
   in
-  assert_pat "_" (make_pat PatWild Span.dummy);
-  assert_pat "x" (make_pat (PatIdent "x") Span.dummy);
-  assert_pat "42" (make_pat (PatLit (LitInt "42")) Span.dummy);
-  assert_pat
+  assert_stmt wrapped expected_stmt
+
+let test_pat_basic () =
+  verify_pat "_" (make_pat PatWild Span.dummy);
+  verify_pat "x" (make_pat (PatIdent "x") Span.dummy);
+  verify_pat "42" (make_pat (PatLit (LitInt "42")) Span.dummy)
+
+let test_pat_struct () =
+  verify_pat
     "(x, y)"
     (make_pat
        (PatLitTuple
@@ -241,7 +251,7 @@ let test_pats () =
           ; make_pat (PatIdent "y") Span.dummy
           ])
        Span.dummy);
-  assert_pat
+  verify_pat
     "[x, y]"
     (make_pat
        (PatLitArray
@@ -250,26 +260,30 @@ let test_pats () =
           ; make_pat (PatIdent "y") Span.dummy
           ])
        Span.dummy);
-  assert_pat
-    "Some(x)"
-    (make_pat
-       (PatVariant ("Some", [], Some (make_pat (PatIdent "x") Span.dummy)))
-       Span.dummy);
-  assert_pat "None" (make_pat (PatIdent "None") Span.dummy);
-  assert_pat
-    "head :: tail"
-    (make_pat
-       (PatCons
-          ( make_pat (PatIdent "head") Span.dummy
-          , make_pat (PatIdent "tail") Span.dummy ))
-       Span.dummy);
-  assert_pat
+  verify_pat
     "Point.{x, y}"
     (make_pat
        (PatLitRecord ("Point", [ { field_name = "x" }; { field_name = "y" } ]))
        Span.dummy)
 
-let test_exprs () =
+let test_pat_variant () =
+  verify_pat
+    "Some(x)"
+    (make_pat
+       (PatVariant ("Some", [], Some (make_pat (PatIdent "x") Span.dummy)))
+       Span.dummy);
+  verify_pat "None" (make_pat (PatIdent "None") Span.dummy)
+
+let test_pat_cons () =
+  verify_pat
+    "head :: tail"
+    (make_pat
+       (PatCons
+          ( make_pat (PatIdent "head") Span.dummy
+          , make_pat (PatIdent "tail") Span.dummy ))
+       Span.dummy)
+
+let test_expr_binary () =
   assert_stmt
     "1 + 2 * 3;"
     (mk_stmt_expr
@@ -296,8 +310,9 @@ let test_exprs () =
              ( mk_expr
                  (ExprBinary (mk_ident "a", Token.QuestionQuestion, mk_ident "b"))
              , Token.QuestionQuestion
-             , mk_ident "c" ))));
+             , mk_ident "c" ))))
 
+let test_expr_range () =
   assert_stmt
     "0..10;"
     (mk_stmt_expr
@@ -307,8 +322,9 @@ let test_exprs () =
   assert_stmt
     "0..;"
     (mk_stmt_expr
-       (mk_expr (ExprRange (mk_lit (LitInt "0"), Token.DotDot, None))));
+       (mk_expr (ExprRange (mk_lit (LitInt "0"), Token.DotDot, None))))
 
+let test_expr_postfix () =
   assert_stmt
     "a.^;"
     (mk_stmt_expr (mk_expr (ExprUnaryPostfix (mk_ident "a", Token.DotCaret))));
@@ -322,20 +338,23 @@ let test_exprs () =
   assert_stmt
     "a[i];"
     (mk_stmt_expr (mk_expr (ExprIndex (mk_ident "a", mk_ident "i"))));
-  assert_stmt "a.b;" (mk_stmt_expr (mk_expr (ExprField (mk_ident "a", "b"))));
+  assert_stmt "a.b;" (mk_stmt_expr (mk_expr (ExprField (mk_ident "a", "b"))))
 
+let test_expr_assign () =
   assert_stmt
     "x <- 1;"
-    (mk_stmt_expr (mk_expr (ExprAssign (mk_ident "x", mk_lit (LitInt "1")))));
+    (mk_stmt_expr (mk_expr (ExprAssign (mk_ident "x", mk_lit (LitInt "1")))))
 
+let test_expr_stmt_bind () =
   let expected_bind =
     mk_stmt_expr
       (mk_expr
          (ExprBind
             (mk_mod, false, "x", None, mk_lit (LitInt "1"), mk_lit (LitInt "0"))))
   in
-  assert_stmt "val x := 1;" expected_bind;
+  assert_stmt "val x := 1;" expected_bind
 
+let test_expr_record_update () =
   let rf =
     {
       field_mutable = false
@@ -347,30 +366,37 @@ let test_exprs () =
   assert_stmt
     ".{ orig with x := 1 };"
     (mk_stmt_expr
-       (mk_expr (ExprLitRecord (None, [ rf ], Some (mk_ident "orig")))));
+       (mk_expr (ExprLitRecord (None, [ rf ], Some (mk_ident "orig")))))
 
+let test_expr_control () =
   assert_stmt
     "return 1;"
     (mk_stmt_expr (mk_expr (ExprReturn (Some (mk_lit (LitInt "1"))))));
   assert_stmt "break;" (mk_stmt_expr (mk_expr (ExprBreak None)));
   assert_stmt
-    "defer { cleanup(); };"
+    "defer cleanup();"
     (mk_stmt_expr
-       (mk_expr
-          (ExprDefer
-             (mk_expr
-                (ExprBlock
-                   ( [
-                       mk_stmt_expr (mk_expr (ExprCall (mk_ident "cleanup", [])))
-                     ]
-                   , None ))))))
+       (mk_expr (ExprDefer (mk_expr (ExprCall (mk_ident "cleanup", []))))))
 
 let test_suite =
   [
-    ("lits", [ test_case "lits" `Quick test_lits ])
-  ; ("types", [ test_case "types" `Quick test_types ])
-  ; ("pats", [ test_case "pats" `Quick test_pats ])
-  ; ("exprs", [ test_case "exprs" `Quick test_exprs ])
+    ("lit_basic", [ test_case "lit_basic" `Quick test_lit_basic ])
+  ; ("lit_nums", [ test_case "lit_nums" `Quick test_lit_numbers ])
+  ; ("ty_basic", [ test_case "ty_basic" `Quick test_ty_basic ])
+  ; ("ty_ptr_opt", [ test_case "ty_ptr_opt" `Quick test_ty_ptr_opt ])
+  ; ("ty_struct", [ test_case "ty_struct" `Quick test_ty_struct ])
+  ; ("ty_fn", [ test_case "ty_fn" `Quick test_ty_fn ])
+  ; ("pat_basic", [ test_case "pat_basic" `Quick test_pat_basic ])
+  ; ("pat_struct", [ test_case "pat_struct" `Quick test_pat_struct ])
+  ; ("pat_variant", [ test_case "pat_variant" `Quick test_pat_variant ])
+  ; ("pat_cons", [ test_case "pat_cons" `Quick test_pat_cons ])
+  ; ("expr_binary", [ test_case "expr_binary" `Quick test_expr_binary ])
+  ; ("expr_range", [ test_case "expr_range" `Quick test_expr_range ])
+  ; ("expr_postfix", [ test_case "expr_postfix" `Quick test_expr_postfix ])
+  ; ("expr_assign", [ test_case "expr_assign" `Quick test_expr_assign ])
+  ; ("expr_bind", [ test_case "expr_bind" `Quick test_expr_stmt_bind ])
+  ; ("expr_record", [ test_case "expr_record" `Quick test_expr_record_update ])
+  ; ("expr_control", [ test_case "expr_control" `Quick test_expr_control ])
   ]
 
 let () = run "parser_tests" test_suite
