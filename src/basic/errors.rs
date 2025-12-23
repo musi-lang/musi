2bef030c6fbb9f6528207958a4fc68f0acba75f9
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-23 21:40:57 +0200

    more adjustments

--- src/basic/errors.rs
+++ src/basic/errors.rs
@@ -1,42 +1,50 @@
 use crate::basic::span::Span;
+use crate::lex::errors::LexErrorKind;
 use thiserror::Error;
 
 pub type MusiResult<T> = Result<T, MusiError>;
 
 #[derive(Debug, Clone, Error)]
 #[error("{kind}")]
+#[non_exhaustive]
 pub struct MusiError {
     pub kind: ErrorKind,
     pub span: Span,
 }
 
 impl MusiError {
+    #[must_use]
     pub const fn new(kind: ErrorKind, span: Span) -> Self {
         Self { kind, span }
     }
 
+    #[must_use]
     pub fn hint(&self) -> Option<&'static str> {
         self.kind.hint()
     }
 
+    #[must_use]
     pub const fn level(&self) -> Level {
         self.kind.level()
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Error)]
+#[non_exhaustive]
 pub enum ErrorKind {
     #[error(transparent)]
-    Lex(#[from] crate::lex::errors::LexErrorKind),
+    Lex(#[from] LexErrorKind),
 }
 
 impl ErrorKind {
-    pub fn hint(&self) -> Option<&'static str> {
+    #[must_use]
+    pub const fn hint(&self) -> Option<&'static str> {
         match self {
             Self::Lex(err) => err.hint(),
         }
     }
 
+    #[must_use]
     pub const fn level(&self) -> Level {
         match self {
             Self::Lex(err) => err.level(),
@@ -45,6 +53,7 @@ impl ErrorKind {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[non_exhaustive]
 pub enum Level {
     Error,
     Warning,
