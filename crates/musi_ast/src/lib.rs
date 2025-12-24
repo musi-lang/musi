e935b59f328a7d7a378609dd8d3772a7f96f8cd6
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-24 21:32:00 +0200

    more stuff

--- crates/musi_ast/src/lib.rs
+++ crates/musi_ast/src/lib.rs
@@ -1,1 +1,20 @@
-//! Placeholder for Musi AST.
+pub mod nodes;
+
+pub use nodes::*;
+
+pub type Ident = u32;
+pub type Idents = Vec<Ident>;
+pub type OptIdent = Option<Ident>;
+
+pub type ExprPtr = Box<Expr>;
+pub type TypPtr = Box<Typ>;
+pub type OptExprPtr = Option<ExprPtr>;
+pub type OptExpr = Option<Expr>;
+pub type OptTyp = Option<Typ>;
+
+pub type ExprList = Vec<Expr>;
+pub type TypList = Vec<Typ>;
+pub type PatList = Vec<Pat>;
+pub type StmtList = Vec<Stmt>;
+pub type AttrList = Vec<Attr>;
+pub type FieldList = Vec<Field>;
---
+++ crates/musi_ast/src/nodes.rs
@@ -0,0 +1,310 @@
+use musi_basic::span::Span;
+use musi_lex::token::TokenKind;
+
+use crate::{
+    AttrList, ExprList, ExprPtr, FieldList, Ident, Idents, OptExpr, OptExprPtr, OptIdent, OptTyp,
+    PatList, StmtList, TypList, TypPtr,
+};
+
+// ============================================================================
+// LITERALS
+// ============================================================================
+
+#[derive(Debug, Clone)]
+pub enum LitKind {
+    Int(i64),
+    Real(f64),
+    String(Ident),
+    Rune(char),
+    Bool(bool),
+    Template(Vec<TemplatePart>),
+}
+
+#[derive(Debug, Clone)]
+pub enum TemplatePart {
+    Text(Ident),
+    Expr(ExprPtr),
+}
+
+// ============================================================================
+// TYPES
+// ============================================================================
+
+#[derive(Debug, Clone)]
+pub struct Typ {
+    pub kind: TypKind,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub enum TypKind {
+    /// `Int`, `String`
+    Ident(Ident),
+    /// `List[Int]`, `Map[String, Int]`
+    App { base: Ident, args: TypList },
+    /// `?Int`
+    Optional(TypPtr),
+    /// `[10]Int`, `[]Int`
+    Array { size: Option<i64>, elem: TypPtr },
+    /// `^Int`
+    Ptr(TypPtr),
+    /// `Int -> String`
+    Fn { param: TypPtr, ret: TypPtr },
+    /// `(Int, String)`
+    Tuple(TypList),
+}
+
+// ============================================================================
+// PATTERNS
+// ============================================================================
+
+#[derive(Debug, Clone)]
+pub struct Pat {
+    pub kind: PatKind,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub enum PatKind {
+    /// `x`, `foo`
+    Ident(Ident),
+    /// `42`, `"hello"`
+    Lit(LitKind),
+    ///`_`
+    Wild,
+    /// `(a, b, c)`
+    Tuple(PatList),
+    /// `[a, b, c]`
+    Array(PatList),
+    /// `Point.{x, y}`
+    Record { ty: OptIdent, fields: Idents },
+    /// `Some(x)`, `None`
+    Variant {
+        name: Ident,
+        ty_args: TypList,
+        args: PatList,
+    },
+    /// `head :: tail`
+    Cons(PatList),
+    /// `a | b`
+    Or(PatList),
+}
+
+// ============================================================================
+// EXPRESSIONS
+// ============================================================================
+
+#[derive(Debug, Clone)]
+pub struct Expr {
+    pub kind: ExprKind,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub enum ExprKind {
+    /// `42`, `"hello"`, `true`
+    Lit(LitKind),
+    /// `x`, `foo`
+    Ident(Ident),
+    /// `(a, b, c)`
+    Tuple(ExprList),
+    /// `[a, b, c]`
+    Array(ExprList),
+    /// `Point.{x := 1, y := 2}`
+    Record { ty: OptIdent, fields: FieldList },
+    /// Block: `{ stmt; stmt; expr }`
+    Block { stmts: StmtList, expr: OptExprPtr },
+    /// `if cond { } else if { } else { }`
+    If {
+        cond: ExprPtr,
+        then_br: ExprPtr,
+        else_br: OptExprPtr,
+    },
+    /// `while cond { }`
+    While { cond: ExprPtr, body: ExprPtr },
+    /// `for pat in iter { }`
+    For {
+        pat: Pat,
+        iter: ExprPtr,
+        body: ExprPtr,
+    },
+    /// `match expr { cases }`
+    Match {
+        scrutinee: ExprPtr,
+        cases: Vec<MatchCase>,
+    },
+    /// `try expr else x { }`
+    Try {
+        expr: ExprPtr,
+        else_binding: OptIdent,
+        else_body: OptExprPtr,
+    },
+    /// `return expr`
+    Return(OptExprPtr),
+    /// `defer expr`
+    Defer(ExprPtr),
+    /// `break expr`
+    Break(OptExprPtr),
+    /// `cycle`
+    Cycle,
+    /// `unsafe { }`
+    Unsafe(ExprPtr),
+    /// `import "path"`
+    Import(Ident),
+    /// `extern "C" { fn foo(); }`
+    Extern { abi: OptIdent, fns: Vec<FnSig> },
+    /// `record Point { x: Int; y: Int }`
+    RecordDef {
+        attrs: AttrList,
+        mods: Modifiers,
+        name: OptIdent,
+        ty_params: Idents,
+        fields: FieldList,
+    },
+    /// `sum Option[T] { case Some(T), case None }`
+    SumDef {
+        attrs: AttrList,
+        mods: Modifiers,
+        name: OptIdent,
+        ty_params: Idents,
+        cases: Vec<SumCase>,
+    },
+    /// `alias Name := Type`
+    Alias {
+        attrs: AttrList,
+        mods: Modifiers,
+        name: Ident,
+        ty_params: Idents,
+        ty: Typ,
+    },
+    /// `fn name(params) { body }`
+    Fn {
+        attrs: AttrList,
+        mods: Modifiers,
+        sig: FnSig,
+        body: ExprPtr,
+    },
+    /// `val x := 1` or `var x := 1`
+    Bind {
+        mods: Modifiers,
+        mutable: bool,
+        pat: Pat,
+        ty: OptTyp,
+        init: ExprPtr,
+    },
+    /// `f(args)`
+    Call { callee: ExprPtr, args: ExprList },
+    /// `arr[i]`
+    Index { base: ExprPtr, index: ExprPtr },
+    /// `obj.field`
+    Field { base: ExprPtr, field: Ident },
+    /// `expr.^`
+    Deref(ExprPtr),
+    /// `-x`, `not x`, `~x`, `@x`
+    Unary { op: TokenKind, operand: ExprPtr },
+    /// `a + b`, `a and b`
+    Binary {
+        op: TokenKind,
+        lhs: ExprPtr,
+        rhs: ExprPtr,
+    },
+    /// `a..b`, `a..<b`
+    Range {
+        start: ExprPtr,
+        end: OptExprPtr,
+        inclusive: bool,
+    },
+    /// `x <- y`
+    Assign { target: ExprPtr, value: ExprPtr },
+}
+
+// ============================================================================
+// STATEMENTS
+// ============================================================================
+
+#[derive(Debug, Clone)]
+pub struct Stmt {
+    pub kind: StmtKind,
+    pub span: Span,
+}
+
+#[derive(Debug, Clone)]
+pub enum StmtKind {
+    /// `expr;`
+    Expr(Expr),
+}
+
+// ============================================================================
+// SUPPORT TYPES
+// ============================================================================
+
+/// `[var] name [: ty] [:= init]`
+#[derive(Debug, Clone)]
+pub struct Field {
+    pub mutable: bool,
+    pub name: Ident,
+    pub ty: OptTyp,
+    pub init: OptExpr,
+}
+
+/// `name[T](params): RetType`
+#[derive(Debug, Clone)]
+pub struct FnSig {
+    pub name: OptIdent,
+    pub ty_params: Idents,
+    pub params: FieldList,
+    pub ret: OptTyp,
+}
+
+/// `case pat if guard => body`
+#[derive(Debug, Clone)]
+pub struct MatchCase {
+    pub pat: Pat,
+    pub guard: OptExpr,
+    pub body: Expr,
+}
+
+/// `case Name[T](fields)`
+#[derive(Debug, Clone)]
+pub struct SumCase {
+    pub name: Ident,
+    pub ty_args: TypList,
+    pub fields: Vec<SumCaseItem>,
+}
+
+#[derive(Debug, Clone)]
+pub enum SumCaseItem {
+    Type(Typ),
+    Field(Field),
+}
+
+/// `[<Name(args)>]`
+#[derive(Debug, Clone)]
+pub struct Attr {
+    pub name: Ident,
+    pub args: Vec<AttrArg>,
+}
+
+/// `name := value` or literal
+#[derive(Debug, Clone)]
+pub struct AttrArg {
+    pub name: OptIdent,
+    pub value: OptExpr,
+    pub lit: Option<LitKind>,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Modifiers {
+    pub exportness: bool,
+    pub externness: (OptIdent, bool),
+    pub unsafeness: bool,
+}
+
+// ============================================================================
+// PROGRAM
+// ============================================================================
+
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub stmts: StmtList,
+}
