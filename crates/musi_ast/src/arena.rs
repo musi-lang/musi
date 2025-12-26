cb5aea800d294b65d654ac4053a9652cbe8ab3d9
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-26 19:46:13 +0200

    clippy

--- crates/musi_ast/src/arena.rs
+++ crates/musi_ast/src/arena.rs
@@ -25,31 +25,31 @@ impl AstArena {
 
     pub fn alloc_ty_expr(&mut self, kind: TyExprKind, span: Span) -> TyExprId {
         let id = NodeId::new(u32::try_from(self.ty_exprs.len()).expect("arena overflow"));
-        self.ty_exprs.alloc(TyExpr::new(id, kind, span));
+        let _ = self.ty_exprs.alloc(TyExpr::new(id, kind, span));
         id
     }
 
     pub fn alloc_pat(&mut self, kind: PatKind, span: Span) -> PatId {
         let id = NodeId::new(u32::try_from(self.pats.len()).expect("arena overflow"));
-        self.pats.alloc(Pat::new(id, kind, span));
+        let _ = self.pats.alloc(Pat::new(id, kind, span));
         id
     }
 
     pub fn alloc_expr(&mut self, kind: ExprKind, span: Span) -> ExprId {
         let id = NodeId::new(u32::try_from(self.exprs.len()).expect("arena overflow"));
-        self.exprs.alloc(Expr::new(id, kind, span));
+        let _ = self.exprs.alloc(Expr::new(id, kind, span));
         id
     }
 
     pub fn alloc_cond(&mut self, kind: CondKind) -> CondId {
         let id = NodeId::new(u32::try_from(self.conds.len()).expect("arena overflow"));
-        self.conds.alloc(Cond::new(id, kind));
+        let _ = self.conds.alloc(Cond::new(id, kind));
         id
     }
 
     pub fn alloc_stmt(&mut self, kind: StmtKind, span: Span) -> StmtId {
         let id = NodeId::new(u32::try_from(self.stmts.len()).expect("arena overflow"));
-        self.stmts.alloc(Stmt::new(id, kind, span));
+        let _ = self.stmts.alloc(Stmt::new(id, kind, span));
         id
     }
 }
