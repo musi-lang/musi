2fba39c99923260ce9c241e8a5ff545c7e81f009
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2025-12-28 19:03:15 +0200

    splits

--- crates/musi_cli/src/options.rs
+++ crates/musi_cli/src/options.rs
@@ -11,6 +11,48 @@ pub enum Target {
     Embedded,
 }
 
+#[allow(clippy::struct_excessive_bools)]
+#[derive(Debug, Clone, Copy, Default, Deserialize)]
+#[serde(rename_all = "camelCase", default)]
+pub struct StrictOptions {
+    pub strict: bool,
+    pub no_implicit_any: bool,
+    pub no_implicit_unit: bool,
+    pub strict_function_types: bool,
+    pub strict_optional_checks: bool,
+    pub no_missing_cases_in_match: bool,
+    pub exact_optional_property_types: bool,
+    pub no_implicit_returns: bool,
+}
+
+#[allow(clippy::struct_excessive_bools)]
+#[derive(Debug, Clone, Copy, Default, Deserialize)]
+#[serde(rename_all = "camelCase", default)]
+pub struct UnusedCodeOptions {
+    pub no_unused_locals: bool,
+    pub no_unused_parameters: bool,
+    pub allow_unreachable_code: bool,
+    pub allow_unused_labels: bool,
+}
+
+#[allow(clippy::struct_excessive_bools)]
+#[derive(Debug, Clone, Copy, Default, Deserialize)]
+#[serde(rename_all = "camelCase", default)]
+pub struct EmitOptions {
+    pub no_emit: bool,
+    pub no_emit_on_error: bool,
+    pub remove_comments: bool,
+    pub composite: bool,
+    pub incremental: bool,
+}
+
+#[derive(Debug, Clone, Copy, Default, Deserialize)]
+#[serde(rename_all = "camelCase", default)]
+pub struct ModuleResolutionOptions {
+    pub force_consistent_casing_in_file_names: bool,
+    pub skip_lib_check: bool,
+}
+
 #[derive(Debug, Clone, Default, Deserialize)]
 #[serde(rename_all = "camelCase", default)]
 pub struct CompilerOptions {
@@ -19,40 +61,29 @@ pub struct CompilerOptions {
     pub out_dir: Option<PathBuf>,
     pub root_dir: Option<PathBuf>,
     pub base_url: Option<PathBuf>,
-    pub strict: bool,
-    pub no_implicit_any: bool,
-    pub no_implicit_unit: bool,
-    pub strict_function_types: bool,
-    pub strict_optional_checks: bool,
-    pub no_missing_cases_in_match: bool,
-    pub exact_optional_property_types: bool,
-    pub no_unused_locals: bool,
-    pub no_unused_parameters: bool,
-    pub no_implicit_returns: bool,
-    pub allow_unreachable_code: bool,
-    pub allow_unused_labels: bool,
-    pub force_consistent_casing_in_file_names: bool,
-    pub skip_lib_check: bool,
-    pub no_emit: bool,
-    pub no_emit_on_error: bool,
-    pub remove_comments: bool,
-    pub composite: bool,
-    pub incremental: bool,
+    #[serde(flatten)]
+    pub strict: StrictOptions,
+    #[serde(flatten)]
+    pub unused: UnusedCodeOptions,
+    #[serde(flatten)]
+    pub emit: EmitOptions,
+    #[serde(flatten)]
+    pub module: ModuleResolutionOptions,
     pub build_info_file: Option<PathBuf>,
 }
 
 impl CompilerOptions {
-    pub fn apply_strict(&mut self) {
-        if self.strict {
-            self.no_implicit_any = true;
-            self.no_implicit_unit = true;
-            self.strict_function_types = true;
-            self.strict_optional_checks = true;
-            self.no_missing_cases_in_match = true;
-            self.exact_optional_property_types = true;
-            self.no_unused_locals = true;
-            self.no_unused_parameters = true;
-            self.no_implicit_returns = true;
+    pub const fn apply_strict(&mut self) {
+        if self.strict.strict {
+            self.strict.no_implicit_any = true;
+            self.strict.no_implicit_unit = true;
+            self.strict.strict_function_types = true;
+            self.strict.strict_optional_checks = true;
+            self.strict.no_missing_cases_in_match = true;
+            self.strict.exact_optional_property_types = true;
+            self.unused.no_unused_locals = true;
+            self.unused.no_unused_parameters = true;
+            self.strict.no_implicit_returns = true;
         }
     }
 }
