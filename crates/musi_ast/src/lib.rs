67ac91f7e0c347953e0fad0ae7556c473cab4530
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-13 22:24:06 +0200

    relocate

--- crates/musi_ast/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod arena;
-pub mod node;
-pub mod types;
-pub mod visitor;
-
-pub use arena::*;
-pub use node::*;
-pub use types::*;
-pub use visitor::*;
--- crates/musi_lex/src/types.rs
+++
@@ -1,6 +0,0 @@
-use musi_core::DiagnosticBag;
-
-use crate::token::Token;
-
-/// Stream of tokens and resulting diagnostics.
-pub type TokenStream = (Vec<Token>, DiagnosticBag);
