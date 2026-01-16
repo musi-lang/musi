1b1f8dc2bb60b646b290c09c0efbaa737fdf14e3
xsyetops <187086553+xsyetopz@users.noreply.github.com>
2026-01-16 10:16:47 +0200

    interner her

---
+++ crates/musi_core/src/interner.rs
@@ -0,0 +1,73 @@
+#[derive(Debug)]
+pub struct Interner {
+    strings: Vec<String>,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Name(pub(crate) u32);
+
+impl Name {
+    #[must_use]
+    pub const fn new(value: u32) -> Self {
+        Self(value)
+    }
+
+    #[must_use]
+    pub const fn as_u32(self) -> u32 {
+        self.0
+    }
+}
+
+impl Interner {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self { strings: vec![] }
+    }
+
+    /// Intern string and return compact identifier
+    ///
+    /// # Panics
+    /// If `Interner` would exceed `u32::MAX` strings
+    pub fn intern(&mut self, s: &str) -> Name {
+        let id = self
+            .strings
+            .iter()
+            .position(|existing| existing == s)
+            .unwrap_or_else(|| {
+                self.strings.push(s.to_owned());
+                self.strings.len() - 1
+            });
+
+        Name(id.try_into().expect("interner overflow: too many strings"))
+    }
+
+    /// Get string slice from interned identifier
+    ///
+    /// # Panics
+    /// If `Name` ID exceeds `usize::MAX`
+    #[must_use]
+    pub fn resolve(&self, name: Name) -> &str {
+        self.strings
+            .get(usize::try_from(name.0).expect("invalid Name ID"))
+            .map_or("", String::as_str)
+    }
+
+    #[must_use]
+    pub const fn is_empty(&self) -> bool {
+        self.strings.is_empty()
+    }
+
+    #[must_use]
+    pub const fn len(&self) -> usize {
+        self.strings.len()
+    }
+}
+
+impl Default for Interner {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests;
