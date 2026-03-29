18fe6e3f0fbb2bf7a3d07c7dd627bc373d580a69
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-03-29 11:05:13 +0300

    remove old

--- crates/music_lex/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod cursor;
-pub mod errors;
-pub mod lexer;
-pub mod token;
-
-pub use errors::{LexError, LexErrorKind, LexResult};
-pub use lexer::Lexer;
-pub use token::{StringFragment, Token, TokenKind, Trivia, TriviaKind, TriviaList};
