cc50660db77182e431f1b25bc54828293b2500a9
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-04-02 06:03:13 +0300

    remove old

--- crates/music_names/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod ident;
-pub mod resolution;
-pub mod symbol;
-
-pub use ident::Ident;
-pub use resolution::{NameBinding, NameBindingId, NameBindingKind, NameResolution, NameSite};
-pub use symbol::{Interner, Symbol};
-
-pub type SymbolSlice = Box<[Symbol]>;
--- crates/music_storage/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod arena;
-mod idx;
-
-pub use arena::Arena;
-pub use idx::Idx;
